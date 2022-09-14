a4b0530d6ce867f0f0c2bc52aafc5dc501a7b7df
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-09-14 11:34:18 +0200

    internal/version: move version information to cmd/forwarder

--- cmd/forwarder/version.go
+++ cmd/forwarder/version.go
@@ -6,9 +6,15 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
-	"github.com/saucelabs/forwarder/internal/version"
 	"github.com/spf13/cobra"
+)
+
+var (
+	buildVersion = "Devel"
+	buildTime    = "Unknown"
+	buildCommit  = "Unknown"
 )
 
 // versionCmd represents the version command.
@@ -16,7 +22,15 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Prints version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Fprintln(cmd.OutOrStdout(), version.Get())
+		w := cmd.OutOrStdout()
+
+		fmt.Fprintln(w, "Version:\t", buildVersion)
+		fmt.Fprintln(w, "Built time:\t", buildTime)
+		fmt.Fprintln(w, "Git commit:\t", buildCommit)
+
+		fmt.Fprintln(w, "Go Arch:\t", runtime.GOARCH)
+		fmt.Fprintln(w, "Go OS:\t\t", runtime.GOOS)
+		fmt.Fprintln(w, "Go Version:\t", runtime.Version())
 	},
 }
 
