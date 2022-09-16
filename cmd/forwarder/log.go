821df17a450e6a742daf95cff9489847cd57aaf4
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-09-16 13:08:53 +0200

    cmd/forwarder: modernize logger configuration
    - Add logConfig struct and defaultLogConfig func
    - Move log configuration to run subcommand and remove persistent flag (also used in version etc.)
    - Add runArgs variable to limit nr of global variables

---
+++ cmd/forwarder/log.go
@@ -0,0 +1,72 @@
+// Copyright 2021 The forwarder Authors. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/saucelabs/sypl"
+	"github.com/saucelabs/sypl/fields"
+	"github.com/saucelabs/sypl/formatter"
+	"github.com/saucelabs/sypl/level"
+	"github.com/saucelabs/sypl/options"
+	"github.com/saucelabs/sypl/output"
+)
+
+const (
+	defaultTimeFormat = "2006-01-02T15-04-05"
+	infoLevel         = "info"
+	name              = "proxy"
+)
+
+// logConfig is a configuration for the logger.
+type logConfig struct {
+	FileLevel string `validate:"required,gte=3"`
+	FilePath  string `validate:"required"`
+	Level     string `validate:"required,gte=3"`
+}
+
+func defaultLogConfig() logConfig {
+	return logConfig{
+		FileLevel: infoLevel,
+		FilePath:  filepath.Join(os.TempDir(), fmt.Sprintf("%s-%s.log", name, time.Now().Format(defaultTimeFormat))),
+		Level:     infoLevel,
+	}
+}
+
+func newLogger(cfg logConfig) forwarderLogger {
+	l := sypl.NewDefault(name, level.MustFromString(cfg.Level))
+	l.AddOutputs(
+		output.File(cfg.FilePath, level.MustFromString(cfg.FileLevel)).SetFormatter(formatter.Text()),
+	)
+	l.PrintlnWithOptions(&options.Options{
+		Fields: fields.Fields{
+			"fileLevel": cfg.FileLevel,
+			"filePath":  cfg.FilePath,
+			"level":     cfg.Level,
+		},
+	}, level.Trace, "Logging is configured")
+	return forwarderLogger{l}
+}
+
+// forwarderLogger is a wrapper around sypl.Logger that implements forwarder.Logger.
+// It is needed because sypl.Logger methods return sypl.ISypl, which makes it impossible work with anything else...
+type forwarderLogger struct {
+	*sypl.Sypl
+}
+
+func (l forwarderLogger) Errorf(format string, args ...interface{}) {
+	l.Sypl.Errorf(format, args...)
+}
+
+func (l forwarderLogger) Infof(format string, args ...interface{}) {
+	l.Sypl.Infof(format, args...)
+}
+
+func (l forwarderLogger) Debugf(format string, args ...interface{}) {
+	l.Sypl.Debugf(format, args...)
+}
