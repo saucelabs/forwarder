87eab117bf9102386441525fbb8d7feaaf2310f1
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-09-16 13:01:40 +0200

    internal/logger: remove

--- internal/logger/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2021 The forwarder Authors. All rights reserved.
-// Use of this source code is governed by a MIT
-// license that can be found in the LICENSE file.
-
-// Package Logger provides a singleton which allows consistent logging across
-// the application.
-package logger
