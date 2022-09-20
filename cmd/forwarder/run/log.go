963fe440ad2e0180b3157fa6dfab66c43173a08a
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-09-20 13:48:01 +0200

    cmd/forwarder: move run command to separate package
    Fixes #61

--- cmd/forwarder/run/log.go
+++ cmd/forwarder/run/log.go
@@ -1,7 +1,8 @@
 // Copyright 2021 The forwarder Authors. All rights reserved.
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
-package main
+
+package run
 
 import (
 	"fmt"
--- cmd/forwarder/run/run.go
+++ cmd/forwarder/run/run.go
@@ -2,25 +2,28 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-package main
+package run
 
 import (
 	"github.com/saucelabs/forwarder"
 	"github.com/spf13/cobra"
 )
 
-var runArgs = struct {
+type command struct {
 	proxyConfig forwarder.ProxyConfig
 	logConfig   logConfig
-}{
-	logConfig: defaultLogConfig(),
 }
 
-// runCmd represents the run command.
-var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "Starts the proxy",
-	Long: `Run starts the proxy. Proxy can be protected with basic auth.
+func (c *command) RunE(cmd *cobra.Command, args []string) error {
+	p, err := forwarder.NewProxy(c.proxyConfig, newLogger(c.logConfig))
+	if err != nil {
+		return err
+	}
+
+	return p.Run()
+}
+
+const long = `Run starts the proxy. Proxy can be protected with basic auth.
 It can forward connections to an upstream proxy (protected, or not).
 The upstream proxy can be automatically setup via PAC (protected, or not).
 Also, credentials for proxies specified in PAC can be set.
@@ -34,8 +37,9 @@ All credentials can be set via env vars:
 - Target URLs: FORWARDER_SITE_CREDENTIALS
 
 Note: Can't setup upstream, and PAC at the same time.
-`,
-	Example: `  Start a proxy listening to http://localhost:8080:
+`
+
+const example = `Start a proxy listening to http://localhost:8080:
   $ forwarder run
 
   Start a proxy listening to http://0.0.0.0:8085:
@@ -74,7 +78,7 @@ Note: Can't setup upstream, and PAC at the same time.
   $ forwarder run \
     -l "http://user:pwd@localhost:8085" \
     -p "http://user2:pwd2@localhost:8090" \
-	  -d "http://user3:pwd4@localhost:8091,http://user4:pwd5@localhost:8092"
+    -d "http://user3:pwd4@localhost:8091,http://user4:pwd5@localhost:8092"
 
   Start a protected proxy, forwarding connection to an upstream proxy, setup via
   PAC - protected server running at http://user2:pwd2@localhost:8090, specifying
@@ -84,37 +88,38 @@ Note: Can't setup upstream, and PAC at the same time.
     -t \
     -l "http://user:pwd@localhost:8085" \
     -p "http://user2:pwd2@localhost:8090" \
-	  -d "http://user3:pwd4@localhost:8091,http://user4:pwd5@localhost:8092"
+    -d "http://user3:pwd4@localhost:8091,http://user4:pwd5@localhost:8092"
 
   Start a protected proxy that adds basic auth header to requests to foo.bar:8090
   and qux.baz:80.
   $ forwarder run \
     -t \
     -l "http://user:pwd@localhost:8085" \
-	--site-credentials "user1:pwd1@foo.bar:8090,user2:pwd2@qux:baz:80"
-	`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		p, err := forwarder.NewProxy(runArgs.proxyConfig, newLogger(runArgs.logConfig))
-		if err != nil {
-			return err
-		}
+    --site-credentials "user1:pwd1@foo.bar:8090,user2:pwd2@qux:baz:80"
+`
 
-		return p.Run()
-	},
-}
-
-func init() {
-	rootCmd.AddCommand(runCmd)
-
-	fs := runCmd.Flags()
-	fs.StringVarP(&runArgs.proxyConfig.LocalProxyURI, "local-proxy-uri", "l", "http://localhost:8080", "sets local proxy URI")
-	fs.StringVarP(&runArgs.proxyConfig.UpstreamProxyURI, "upstream-proxy-uri", "u", "", "sets upstream proxy URI")
-	fs.StringSliceVarP(&runArgs.proxyConfig.DNSURIs, "dns-uri", "n", nil, "sets dns URI")
-	fs.StringVarP(&runArgs.proxyConfig.PACURI, "pac-uri", "p", "", "sets URI to PAC content, or directly, the PAC content")
-	fs.StringSliceVarP(&runArgs.proxyConfig.PACProxiesCredentials, "pac-proxies-credentials", "d", nil, "sets PAC proxies credentials using standard URI format")
-	fs.StringSliceVar(&runArgs.proxyConfig.SiteCredentials, "site-credentials", nil, "sets target site credentials")
-	fs.BoolVarP(&runArgs.proxyConfig.ProxyLocalhost, "proxy-localhost", "t", false, "if set, will proxy localhost requests to an upstream proxy - if any")
-	fs.StringVar(&runArgs.logConfig.Level, "log-level", runArgs.logConfig.Level, "sets the log level (default info)")
-	fs.StringVar(&runArgs.logConfig.FileLevel, "log-file-level", runArgs.logConfig.FileLevel, "sets the log file level (default info)")
-	fs.StringVar(&runArgs.logConfig.FilePath, "log-file-path", runArgs.logConfig.FilePath, `sets the log file path (default "OS temp dir")`)
+func Command() (cmd *cobra.Command) {
+	c := command{
+		logConfig: defaultLogConfig(),
+	}
+	defer func() {
+		fs := cmd.Flags()
+		fs.StringVarP(&c.proxyConfig.LocalProxyURI, "local-proxy-uri", "l", "http://localhost:8080", "sets local proxy URI")
+		fs.StringVarP(&c.proxyConfig.UpstreamProxyURI, "upstream-proxy-uri", "u", "", "sets upstream proxy URI")
+		fs.StringSliceVarP(&c.proxyConfig.DNSURIs, "dns-uri", "n", nil, "sets dns URI")
+		fs.StringVarP(&c.proxyConfig.PACURI, "pac-uri", "p", "", "sets URI to PAC content, or directly, the PAC content")
+		fs.StringSliceVarP(&c.proxyConfig.PACProxiesCredentials, "pac-proxies-credentials", "d", nil, "sets PAC proxies credentials using standard URI format")
+		fs.StringSliceVar(&c.proxyConfig.SiteCredentials, "site-credentials", nil, "sets target site credentials")
+		fs.BoolVarP(&c.proxyConfig.ProxyLocalhost, "proxy-localhost", "t", false, "if set, will proxy localhost requests to an upstream proxy - if any")
+		fs.StringVar(&c.logConfig.Level, "log-level", c.logConfig.Level, "sets the log level (default info)")
+		fs.StringVar(&c.logConfig.FileLevel, "log-file-level", c.logConfig.FileLevel, "sets the log file level (default info)")
+		fs.StringVar(&c.logConfig.FilePath, "log-file-path", c.logConfig.FilePath, `sets the log file path (default "OS temp dir")`)
+	}()
+	return &cobra.Command{
+		Use:     "run",
+		Short:   "Starts the proxy",
+		Long:    long,
+		Example: example,
+		RunE:    c.RunE,
+	}
 }
