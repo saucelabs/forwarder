04bcebe9ed6be02d80b8b6e708ecf07ff255ce01
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-09-20 14:54:31 +0200

    cmd/forwarder: bind flag values from environment variables
    Example:
    
    ```
    Flags:
      -n, --dns-uri strings                   sets dns URI (env FORWARDER_DNS_URI)
      -h, --help                              help for run
      -l, --local-proxy-uri string            sets local proxy URI (env FORWARDER_LOCAL_PROXY_URI) (default "http://localhost:8080")
          --log-file-level string             sets the log file level (env FORWARDER_LOG_FILE_LEVEL) (default "info")
          --log-file-path string              sets the log file path (env FORWARDER_LOG_FILE_PATH) (default "/var/folders/7q/nyynjpwj5p19npby48ykjpx00000gn/T/proxy-2022-09-20T15-23-53.log")
          --log-level string                  sets the log level (env FORWARDER_LOG_LEVEL) (default "info")
      -d, --pac-proxies-credentials strings   sets PAC proxies credentials using standard URI format (env FORWARDER_PAC_PROXIES_CREDENTIALS)
      -p, --pac-uri string                    sets URI to PAC content, or directly, the PAC content (env FORWARDER_PAC_URI)
      -t, --proxy-localhost                   if set, will proxy localhost requests to an upstream proxy - if any (env FORWARDER_PROXY_LOCALHOST)
          --site-credentials strings          sets target site credentials (env FORWARDER_SITE_CREDENTIALS)
      -u, --upstream-proxy-uri string         sets upstream proxy URI (env FORWARDER_UPSTREAM_PROXY_URI)
    ```
    
    - Usage is appended with the environment variable name
    - Binding is automated on root command level meaning it can be enabled or disabled for a particular command

---
+++ cmd/forwarder/env.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+// appendEnvToUsage appends the environment variable name to the usage string of each Cobra flag.
+func appendEnvToUsage(cmd *cobra.Command, envPrefix string) {
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		f.Usage += fmt.Sprintf(" (env %s)", envName(envPrefix, f.Name))
+	})
+}
+
+// bindFlagsToEnv binds each Cobra flag to its associated Viper configuration (config file and environment variable).
+func bindFlagsToEnv(cmd *cobra.Command, envPrefix string) error {
+	v := viper.New()
+
+	var bindErr error
+	fs := cmd.Flags()
+	fs.VisitAll(func(f *pflag.Flag) {
+		// Bind environment variable to flag
+		if err := v.BindEnv(f.Name, envName(envPrefix, f.Name)); err != nil {
+			bindErr = err
+			return
+		}
+
+		// Set default value from environment variable
+		if !f.Changed && v.IsSet(f.Name) {
+			if err := fs.Set(f.Name, fmt.Sprintf("%v", v.Get(f.Name))); err != nil {
+				bindErr = err
+				return
+			}
+		}
+	})
+	return bindErr
+}
+
+func envName(envPrefix, flagName string) string {
+	name := flagName
+	name = strings.ReplaceAll(name, "-", "_")
+	name = strings.ToUpper(name)
+	return fmt.Sprintf("%s_%s", envPrefix, name)
+}
