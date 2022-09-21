628d623973a26054f3b5ab055918951e1807d00f
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-09-21 13:41:55 +0200

    doc: remove information on env variables in forwarder package

--- doc.go
+++ doc.go
@@ -6,6 +6,4 @@
 // The proxy can be protected with HTTP basic authentication.
 // It can also forward connections to a parent proxy, and authorize connections against that.
 // Both local, and parent credentials can be set via environment variables.
-// For local proxy credential, set `FORWARDER_LOCALPROXY_AUTH`.
-// For parent proxy credential, set `FORWARDER_UPSTREAMPROXY_AUTH`.
 package forwarder
