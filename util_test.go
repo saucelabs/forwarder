3a0ee96dbf4de63af2af3c4ed72fb8b890ee2b28
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-09-28 09:41:37 +0200

    proxy: harden localhost proxying
    ProxyLocalhost:
    * disabled - return 402 Bad Gateway
    * enabled - forward upstream or call localhost
    
    This patch fixes the way the localhost proxying is set up, and avoids reconfiguring goproxy on each request (for localhost).
    It's part of larger work to fix the init process as described in #29.
    
    The util functions to recognize localhost are removed as we use library functions.
    
    Fixes #76

--- util_test.go
+++ util_test.go
@@ -5,7 +5,6 @@
 package forwarder
 
 import (
-	"net/http"
 	"net/url"
 	"testing"
 )
@@ -57,53 +56,6 @@ func TestNormalizeURLScheme(t *testing.T) {
 			}
 			if u.String() != tc.expected {
 				t.Errorf("expected %s, got %s", tc.expected, u)
-			}
-		})
-	}
-}
-
-func TestIsLocalhost(t *testing.T) {
-	tests := []struct {
-		name     string
-		url      string
-		expected bool
-	}{
-		{
-			name:     "Is localhost",
-			url:      "http://localhost:8080",
-			expected: true,
-		},
-		{
-			name:     "Is localhost IPv6",
-			url:      "http://[::1]:8080",
-			expected: true,
-		},
-		{
-			name:     "Is localhost IPv6 no port",
-			url:      "http://[::1]",
-			expected: true,
-		},
-		{
-			name:     "Is localhost IPv4",
-			url:      "http://127.0.0.2:8080",
-			expected: true,
-		},
-		{
-			name:     "Not localhost",
-			url:      "example.com:8888",
-			expected: false,
-		},
-	}
-
-	for i := range tests {
-		tc := tests[i]
-		t.Run(tc.name, func(t *testing.T) {
-			r, err := http.NewRequest(http.MethodGet, tc.url, http.NoBody)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if isLocalhost(r.URL.Hostname()) != tc.expected {
-				t.Errorf("expected %v, got %v", tc.expected, isLocalhost(r.URL.Hostname()))
 			}
 		})
 	}
