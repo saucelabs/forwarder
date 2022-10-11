31dd29b85fadf3bd282a1289a0d2c0cd7b9a97da
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-10-11 12:36:00 +0200

    basic auth: add BasicAuthUtil a standalone basic auth implementation with ability to set the authentication header
    BasicAuthUtil exposes common Basic Authentication functionalities from the standard library, and allows to customize the Authentication header.
    This is useful when you want to use Basic Authentication for a proxy.
    
    BasicAuthUtil is more versatile than the ext/auth plugin out of the box it supports
    * Constant-time credentials check
    * Credentials hiding from upstream servers / backends
    * Middleware with Proxy support

---
+++ basic_auth_test.go
@@ -0,0 +1,52 @@
+package forwarder
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestBasicAuthUtil(t *testing.T) {
+	ba := &BasicAuthUtil{Header: "Foo"}
+	r := httptest.NewRequest("GET", "/", nil)
+	ba.SetBasicAuth(r, "user", "pass")
+
+	if user, pass, ok := ba.BasicAuth(r); !ok || user != "user" || pass != "pass" {
+		t.Errorf("BasicAuth failed, got %v %v %v", user, pass, ok)
+	}
+	if !ba.AuthenticatedRequest(r, "user", "pass") {
+		t.Errorf("AuthenticatedRequest failed")
+	}
+}
+
+func TestBasicAuthUtilWrap(t *testing.T) {
+	ba := &BasicAuthUtil{Header: "Foo"}
+
+	h := ba.Wrap(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Foo") != "" {
+			t.Errorf("Auth header should not be forwarded")
+		}
+		w.WriteHeader(http.StatusOK)
+	}), "user", "pass")
+
+	t.Run("Authenticated", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		ba.SetBasicAuth(r, "user", "pass")
+
+		h.ServeHTTP(w, r)
+		if w.Result().StatusCode != http.StatusOK {
+			t.Errorf("Authenticated failed, got %v", w.Result().StatusCode)
+		}
+	})
+
+	t.Run("Not Authenticated", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		h.ServeHTTP(w, r)
+		if w.Result().StatusCode != http.StatusUnauthorized {
+			t.Errorf("Authenticated failed, got %v", w.Result().StatusCode)
+		}
+	})
+}
