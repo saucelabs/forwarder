fd6ef0e2bcdc75fc39c73e0f3d75b49669603f11
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-10-13 13:42:02 +0200

    bind: HTTPServerConfig, change addr to address for clarity

--- cmd/forwarder/proxy/proxy.go
+++ cmd/forwarder/proxy/proxy.go
@@ -55,25 +55,25 @@ It supports custom DNS servers.
 `
 
 const example = `Start HTTP proxy listening to localhost:8080:
-  $ forwarder proxy --addr localhost:8080
+  $ forwarder proxy --address localhost:8080
 
   Start a protected proxy protected with basic auth:
-  $ forwarder proxy --addr localhost:8080 --basic-auth user:pass
+  $ forwarder proxy --address localhost:8080 --basic-auth user:pass
 
   Forward connections to an upstream proxy:
-  $ forwarder proxy --addr localhost:8080 --upstream-proxy-uri http://localhost:8089
+  $ forwarder proxy --address localhost:8080 --upstream-proxy-uri http://localhost:8089
 
   Forward connections to an upstream proxy protected with basic auth:
-  $ forwarder proxy --addr localhost:8080 --upstream-proxy-uri http://localhost:8089 --upstream-proxy-basic-auth user:pass
+  $ forwarder proxy --address localhost:8080 --upstream-proxy-uri http://localhost:8089 --upstream-proxy-basic-auth user:pass
 
   Forward connections to an upstream proxy setup via PAC: 
-  $ forwarder proxy --addr localhost:8080 --pac-uri http://localhost:8090/pac
+  $ forwarder proxy --address localhost:8080 --pac-uri http://localhost:8090/pac
 
   Forward connections to an upstream proxy, setup via PAC protected with basic auth:
-  $ forwarder proxy --addr localhost:8080 --pac-uri http://user:pass@localhost:8090/pac -d http://user3:pwd4@localhost:8091 -d http://user2:pwd2@localhost:8092 
+  $ forwarder proxy --address localhost:8080 --pac-uri http://user:pass@localhost:8090/pac -d http://user3:pwd4@localhost:8091 -d http://user2:pwd2@localhost:8092 
 
   Add basic auth header to requests to foo.bar:* and qux.baz:80.
-  $ forwarder proxy --addr localhost:8080 --site-credentials "foo.bar:0,qux.baz:80"
+  $ forwarder proxy --address localhost:8080 --site-credentials "foo.bar:0,qux.baz:80"
 `
 
 func Command() (cmd *cobra.Command) {
