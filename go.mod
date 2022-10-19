73e8e7dedf5ab5e17c6c86b8710cf5b018b57688
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-10-19 14:01:57 +0200

    pac: add ProxyResolver the PAC runtime
    - The implementation is modeled after chromium-libpac
    - Under the hood uses Goja JavaScript VM
    - Supports Mozilla FindProxyForURL [1]
    - Supports Microsoft IPv6 extensions FindProxyForURLEx [2]
    
    [1] https://developer.mozilla.org/en-US/docs/Web/HTTP/Proxy_servers_and_tunneling/Proxy_Auto-Configuration_PAC_file
    [2] https://learn.microsoft.com/en-us/windows/win32/winhttp/ipv6-extensions-to-navigator-auto-config-file-format

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/saucelabs/forwarder
 go 1.19
 
 require (
+	github.com/dop251/goja v0.0.0-20221003171542-5ea1285e6c91
 	github.com/elazarl/goproxy v0.0.0-20220901064549-fbd10ff4f5a1
 	github.com/google/go-cmp v0.5.9
 	github.com/mmatczuk/anyflag v0.0.0-20221011091730-9f8baeb6fd9c
@@ -14,6 +15,8 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.10.0
 	go.uber.org/atomic v1.10.0
+	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
 )
 
 require (
@@ -21,7 +24,6 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dlclark/regexp2 v1.7.0 // indirect
-	github.com/dop251/goja v0.0.0-20221003171542-5ea1285e6c91 // indirect
 	github.com/elazarl/goproxy/ext v0.0.0-20220529153421-8ea89ba92021 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
@@ -51,7 +53,6 @@ require (
 	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
-	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/sys v0.0.0-20220808155132-1c4a2a72c664 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
---
+++ pac/pac_ipv6.go
@@ -0,0 +1,152 @@
+package pac
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net"
+	"sort"
+
+	"github.com/dop251/goja"
+)
+
+// This file implements the Microsoft extensions to PAC for IPv6.
+// See https://learn.microsoft.com/en-us/windows/win32/winhttp/ipv6-aware-proxy-helper-api-definitions
+
+// Handler for "isResolvableEx(host)".
+// Returns TRUE if the host is resolvable to a IPv4 or IPv6 address; otherwise, FALSE.
+// See https://learn.microsoft.com/en-us/windows/win32/winhttp/isresolvableex
+func (pr *ProxyResolver) isResolvableEx(call goja.FunctionCall) goja.Value {
+	return pr.vm.ToValue(pr.dnsResolveEx(call).String() != "")
+}
+
+// Handler for "isInNetEx(host, cidr);".
+// Returns TRUE if the host is in the same subnet; otherwise, FALSE.
+// See https://learn.microsoft.com/en-us/windows/win32/winhttp/isinnetex
+func (pr *ProxyResolver) isInNetEx(call goja.FunctionCall) goja.Value {
+	if isNullOrUndefined(call.Argument(0)) {
+		return goja.Null()
+	}
+	host, ok := asString(call.Argument(0))
+	if !ok {
+		return pr.vm.ToValue(false)
+	}
+
+	if isNullOrUndefined(call.Argument(1)) {
+		return goja.Null()
+	}
+	cidr, ok := asString(call.Argument(1))
+	if !ok {
+		return pr.vm.ToValue(false)
+	}
+
+	ip := net.ParseIP(host)
+	if ip == nil {
+		return pr.vm.ToValue(false)
+	}
+	_, mask, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return pr.vm.ToValue(false)
+	}
+
+	return pr.vm.ToValue(mask.Contains(ip))
+}
+
+// Handler for "dnsResolveEx(host)".
+// Returns a semicolon delimited string containing IPv6 and IPv4 addresses or an empty string if host is not resolvable.
+// See https://learn.microsoft.com/en-us/windows/win32/winhttp/dnsresolveex
+func (pr *ProxyResolver) dnsResolveEx(call goja.FunctionCall) goja.Value {
+	if isNullOrUndefined(call.Argument(0)) {
+		return goja.Null()
+	}
+	host, ok := asString(call.Argument(0))
+	if !ok {
+		return pr.vm.ToValue(false)
+	}
+
+	lookupIP := pr.config.testingLookupIP
+	if lookupIP == nil {
+		lookupIP = pr.resolver.LookupIP
+	}
+	ips, err := lookupIP(context.Background(), "ip", host)
+	if err != nil {
+		return pr.vm.ToValue("")
+	}
+
+	return pr.vm.ToValue(semicolonDelimitedString(ips))
+}
+
+// Handler for "myIpAddressEx()".
+// Returns a semicolon delimited string containing all IP addresses for localhost (IPv6 and/or IPv4), or an empty string if unable to resolve localhost to an IP address.
+// See https://learn.microsoft.com/en-us/windows/win32/winhttp/myipaddressex
+func (pr *ProxyResolver) myIPAddressEx(call goja.FunctionCall) goja.Value {
+	var ips []net.IP
+	if pr.config.testingMyIPAddressEx != nil {
+		ips = pr.config.testingMyIPAddressEx
+	} else {
+		ips = myIPAddress(true)
+	}
+
+	if len(ips) == 0 {
+		return pr.vm.ToValue("")
+	}
+
+	return pr.vm.ToValue(semicolonDelimitedString(ips))
+}
+
+type parsedIP struct {
+	net.IP
+	orig string
+}
+
+func (p parsedIP) String() string {
+	return p.orig
+}
+
+// Handler for "sortIpAddressList(ipAddressList)".
+// Returns a list of sorted semicolon delimited IP addresses or an empty string if unable to sort the IP Address list.
+// If the IP Address list contains both IPv4 and IPv6 addresses, the IPv6 addresses are returned first.
+// https://learn.microsoft.com/en-us/windows/win32/winhttp/sortipaddresslist
+func (pr *ProxyResolver) sortIPAddressList(call goja.FunctionCall) goja.Value {
+	if isNullOrUndefined(call.Argument(0)) {
+		return goja.Null()
+	}
+	s, ok := asString(call.Argument(0))
+	if !ok {
+		return pr.vm.ToValue(false)
+	}
+
+	ips, err := asSlice(s, ";", func(v string) (parsedIP, error) {
+		ip := net.ParseIP(v)
+		if ip == nil {
+			return parsedIP{}, fmt.Errorf("invalid IP address")
+		}
+		return parsedIP{
+			IP:   ip,
+			orig: v,
+		}, nil
+	})
+	if err != nil {
+		return pr.vm.ToValue(false)
+	}
+	if len(ips) == 0 {
+		return pr.vm.ToValue(false)
+	}
+
+	sort.Slice(ips, func(i, j int) bool {
+		// Compare IPv4 to IPv4 and IPv6 to IPv6
+		if (ips[i].To4() != nil) == (ips[j].To4() != nil) {
+			return bytes.Compare(ips[i].IP, ips[j].IP) < 0
+		}
+		// Put IPv6 addresses first.
+		return ips[i].To4() == nil
+	})
+	return pr.vm.ToValue(semicolonDelimitedString(ips))
+}
+
+// Handler for "getClientVersion()".
+// Returns the appropriate versions number of the WPAD engine, currently 1.0.
+// See https://learn.microsoft.com/en-us/windows/win32/winhttp/getclientversion
+func (pr *ProxyResolver) getClientVersion(call goja.FunctionCall) goja.Value {
+	return pr.vm.ToValue("1.0")
+}
