dbcdfeeed944ee6ed770450c10da3af1884a9f8e
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-10-24 17:44:41 +0200

    pac: add SupportedFunctions

--- pac/doc.go
+++ pac/doc.go
@@ -2,3 +2,37 @@
 // Under the hood uses Goja JavaScript VM to run the PAC script.
 // It supports Mozilla FindProxyForURL and the Microsoft IPv6 extension FindProxyForURLEx as well as all the helper functions as described in the PAC specification.
 package pac
+
+import (
+	"regexp"
+	"sort"
+)
+
+var jsFunctionRegex = regexp.MustCompile(`function\s+([a-zA-Z0-9_]+)\s*\(`)
+
+// SupportedFunctions returns a list of supported javascript functions from the PAC specification.
+func SupportedFunctions() []string {
+	var all []string //nolint:prealloc // not worth it
+	for _, m := range jsFunctionRegex.FindAllStringSubmatch(asciiPacUtilsScript, -1) {
+		all = append(all, m[1])
+	}
+
+	// Add built-in functions.
+	all = append(all,
+		"dnsResolve",
+		"myIpAddress",
+		// IPv6
+		"isResolvableEx",
+		"isInNetEx",
+		"dnsResolveEx",
+		"myIpAddressEx",
+		"sortIpAddressList",
+		"getClientVersion",
+		// Alert
+		"alert",
+	)
+
+	sort.Strings(all)
+
+	return all
+}
