df1d6659532b0a46a62baa472cb5002baed61023
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-10-24 12:16:31 +0200

    readurl: add helper function for reading local files or http(s) resources from URL

---
+++ readurl.go
@@ -0,0 +1,84 @@
+package forwarder
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+// ReadURL can read a local file, http or https URL or stdin.
+func ReadURL(u *url.URL, rt http.RoundTripper) (string, error) {
+	switch u.Scheme {
+	case "file":
+		return readFile(u)
+	case "http", "https":
+		return readHTTP(u, rt)
+	default:
+		return "", fmt.Errorf("unsupported scheme %q, supported schemes are file, http and https", u.Scheme)
+	}
+}
+
+func readFile(u *url.URL) (string, error) {
+	if u.Host != "" {
+		return "", fmt.Errorf("invalid file URL %q, host is not allowed", u.String())
+	}
+	if u.User != nil {
+		return "", fmt.Errorf("invalid file URL %q, user is not allowed", u.String())
+	}
+	if u.RawQuery != "" {
+		return "", fmt.Errorf("invalid file URL %q, query is not allowed", u.String())
+	}
+	if u.Fragment != "" {
+		return "", fmt.Errorf("invalid file URL %q, fragment is not allowed", u.String())
+	}
+	if u.Path == "" {
+		return "", fmt.Errorf("invalid file URL %q, path is empty", u.String())
+	}
+
+	if u.Path == "-" {
+		return readAndClose(os.Stdin)
+	}
+
+	f, err := os.Open(u.Path)
+	if err != nil {
+		return "", err
+	}
+	return readAndClose(f)
+}
+
+func readAndClose(r io.ReadCloser) (string, error) {
+	defer r.Close()
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func readHTTP(u *url.URL, rt http.RoundTripper) (string, error) {
+	c := http.Client{
+		Transport: rt,
+	}
+	req, err := http.NewRequest(http.MethodGet, u.String(), http.NoBody) //nolint:noctx // timeout is set in the transport
+	if err != nil {
+		return "", err
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
+}
