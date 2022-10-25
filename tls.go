b90f02f898e686916ad336ea306c829b444a7847
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-10-25 14:48:12 +0200

    allow skipping TLS verification for client connections
    Commands with HTTP transport get new --insecure-skip-verify flag.
    
    Relates to #47

---
+++ tls.go
@@ -0,0 +1,11 @@
+package forwarder
+
+type TLSConfig struct {
+	// InsecureSkipVerify controls whether a client verifies the server's
+	// certificate chain and host name. If InsecureSkipVerify is true, crypto/tls
+	// accepts any certificate presented by the server and any host name in that
+	// certificate. In this mode, TLS is susceptible to machine-in-the-middle
+	// attacks unless custom verification is used. This should be used only for
+	// testing or in combination with VerifyConnection or VerifyPeerCertificate.
+	InsecureSkipVerify bool `json:"insecure_skip_verify"`
+}
