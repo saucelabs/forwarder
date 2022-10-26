67842b2d0654a66ba8d3e822961d186bcf50314b
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-10-26 12:06:03 +0200

    pac: add SOCKS5 mode

--- pac/mode_string.go
+++ pac/mode_string.go
@@ -10,15 +10,16 @@ func _() {
 	var x [1]struct{}
 	_ = x[DIRECT-0]
 	_ = x[PROXY-1]
-	_ = x[SOCKS-2]
-	_ = x[HTTP-3]
-	_ = x[HTTPS-4]
+	_ = x[HTTP-2]
+	_ = x[HTTPS-3]
+	_ = x[SOCKS-4]
 	_ = x[SOCKS4-5]
+	_ = x[SOCKS5-6]
 }
 
-const _Mode_name = "DIRECTPROXYSOCKSHTTPHTTPSSOCKS4"
+const _Mode_name = "DIRECTPROXYHTTPHTTPSSOCKSSOCKS4SOCKS5"
 
-var _Mode_index = [...]uint8{0, 6, 11, 16, 20, 25, 31}
+var _Mode_index = [...]uint8{0, 6, 11, 15, 20, 25, 31, 37}
 
 func (i Mode) String() string {
 	if i < 0 || i >= Mode(len(_Mode_index)-1) {
