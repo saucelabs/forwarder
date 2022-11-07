b8f2ac701c0d98457f42802c33bcbd6007fa1960
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-11-07 12:26:23 +0100

    e2e: add upstream test with upstream using HTTP and HTTPS

--- e2e/run.sh
+++ e2e/run.sh
@@ -21,3 +21,18 @@ run_direct_test http http
 run_direct_test https http
 run_direct_test https https
 run_direct_test http https
+
+run_upstream_test() {
+  trap 'on_error' ERR
+
+  UPSTREAM_PROXY_SCHEME=$1
+  PROXY_SCHEME="http"
+  HTTPBIN_SCHEME="http"
+
+  echo ">>> UPSTREAM proxy=${PROXY_SCHEME} upstream=${UPSTREAM_PROXY_SCHEME} httpbin=${HTTPBIN_SCHEME}"
+  make up CONF="override/proxy-${PROXY_SCHEME}.yaml override/upstream-proxy-${UPSTREAM_PROXY_SCHEME}.yaml httpbin-${HTTPBIN_SCHEME}.yaml"
+  make test ARGS="-proxy ${PROXY_SCHEME}://proxy:3128 -httpbin ${HTTPBIN_SCHEME}://httpbin -insecure-skip-verify"
+}
+
+run_upstream_test http
+run_upstream_test https
