797f7e5edef59946392bf5fdfdeb50a0ed3cc48f
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-11-07 16:17:57 +0100

    add ReadHostsFile a function to read /etc/hosts content

---
+++ hosts.go
@@ -0,0 +1,35 @@
+package forwarder
+
+import (
+	"bytes"
+	"net"
+	"os"
+
+	hostsfile "github.com/kevinburke/hostsfile/lib"
+)
+
+// ReadHostsFile reads the /etc/hosts file and returns a map of hostnames to IP addresses.
+// If the file does not exist, a nil map is returned.
+func ReadHostsFile() (map[string]net.IP, error) {
+	b, err := os.ReadFile(hostsfile.Location)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = nil
+		}
+		return nil, err
+	}
+
+	lines, err := hostsfile.Decode(bytes.NewReader(b))
+	if err != nil {
+		return nil, err
+	}
+
+	m := make(map[string]net.IP, len(lines.Records()))
+	for _, r := range lines.Records() {
+		for h := range r.Hostnames {
+			m[h] = r.IpAddress.IP
+		}
+	}
+
+	return m, nil
+}
