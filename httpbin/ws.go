df99df5488f33bb38287dc0c559e032a544a3157
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-11-29 14:52:49 +0100

    httpbin: add WebSocket echo endpoint
    WebSocket echo server based on [1].
    
    [1] https://github.com/gorilla/websocket/tree/master/examples/echo

---
+++ httpbin/ws.go
@@ -0,0 +1,32 @@
+package httpbin
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func wsEcho(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{} // use default options
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
