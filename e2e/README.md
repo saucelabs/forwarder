6e2aca8ab165f5eae2f1dd06bdb1623c7b2f7945
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-12-05 11:57:32 +0100

    e2e: add tools for benchmarking proxy

--- e2e/README.md
+++ e2e/README.md
@@ -9,6 +9,13 @@
 
 Once the test is complete you may also run curl from the proxy container ex. `docker-compose exec proxy curl -vvv --insecure --proxy-insecure --proxy https://localhost:3128 https://httpbin/status/200`
 
+## Benchmarking
+
+1. Add benchmark to `bench_test.go` or use an existing one
+1. Load test functions `source ./lib.sh`
+1. Run the `run_bench` function ex. `RUN=BenchmarkRespBody1k run_bench http http` it will output the profile directly to the console
+1. Run pprof with the profile output ex. `go tool pprof -http=:8080 /path/to/profiles/cpu`  
+
 ## Test development
 
 1. Run ./dev.sh to start the test environment with proxy and httpbin running on HTTP
--- e2e/lib.sh
+++ e2e/lib.sh
@@ -4,6 +4,12 @@ on_error() {
   make dump-logs
 }
 
+# Profile specifies the number of seconds to run CPU profiling for.
+PROFILE=0
+
+# ARGS can be used to pass additional arguments to the e2e test.
+ARGS=""
+
 run_test() {
   CI=${CI:-""}
   if [[ -n "${CI}" ]]; then
@@ -24,12 +30,36 @@ run_test() {
   fi
   make up CONF="${CONF}"
 
+  if [[ ${PROFILE} -gt 0 ]]; then
+    TMPDIR=$(mktemp -d -t "com.saucelabs.Forwarder.XXXXXX")
+    echo ">>> Profiling enabled output in \"${TMPDIR}\""
+    curl -sS "http://localhost:10000/debug/pprof/profile?seconds=${PROFILE}" -o "${TMPDIR}/cpu" &
+  fi
+
   if [[ "${HTTPBIN_SCHEME}" == "h2" ]]; then
     HTTPBIN_SCHEME="https"
   fi
   if [[ "${PROXY_SCHEME}" == "h2" ]]; then
     PROXY_SCHEME="https"
   fi
+  make test ARGS="${ARGS} -httpbin ${HTTPBIN_SCHEME}://httpbin -proxy ${PROXY_SCHEME}://proxy:3128 -insecure-skip-verify"
 
-  make test ARGS="-httpbin ${HTTPBIN_SCHEME}://httpbin -proxy ${PROXY_SCHEME}://proxy:3128 -insecure-skip-verify"
+  if [[ ${PROFILE} -gt 0 ]]; then
+    curl -sS "http://localhost:10000/debug/pprof/allocs" -o "${TMPDIR}/allocs"
+    curl -sS "http://localhost:10000/debug/pprof/heap" -o "${TMPDIR}/heap"
+    curl -sS "http://localhost:10000/debug/pprof/mutex" -o "${TMPDIR}/mutex"
+  fi
+}
+
+run_bench() {
+  if [[ -z "${RUN}" ]]; then
+    echo "RUN is not set, skipping benchmark"
+    exit 1
+  fi
+  if [[ ${PROFILE} -eq 0 ]]; then
+    PROFILE=10
+  fi
+  ARGS="-test.bench ${RUN} -test.benchtime ${PROFILE}s -test.v"
+  RUN="^XXX"
+  run_test "$@"
 }
