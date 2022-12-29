9241db23fc7fee67c4a5ec0759de4a9853332099
Hubert Grochowski <hubert.grochowski@saucelabs.com>
2022-12-29 16:13:21 +0100

    chore: add linter to guard copyright header
    Fixes #149

--- hosts_test.go
+++ hosts_test.go
@@ -1,3 +1,7 @@
+// Copyright 2022 The forwarder Authors. All rights reserved.
+// Use of this source code is governed by a MPL
+// license that can be found in the LICENSE file.
+
 package forwarder
 
 import "testing"
