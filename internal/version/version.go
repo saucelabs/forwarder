3a7ef365abfd65d019f7252ee247265037e52725
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-02-21 15:38:33 +0100

    license: apply MPL file header
    Fixes #235

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,6 +1,6 @@
-// Copyright 2022 The forwarder Authors. All rights reserved.
-// Use of this source code is governed by a MPL
-// license that can be found in the LICENSE file.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 package version
 
