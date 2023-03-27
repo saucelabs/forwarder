17392706e3e0d2b105c6824b95f4ea713a142650
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-03-27 17:00:06 +0200

    http_proxy: Remove header removal
    Header removal will be handled by custom modifiers in the future.

--- http_proxy_test.go
+++ http_proxy_test.go
@@ -13,7 +13,6 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/saucelabs/forwarder/log/stdlog"
 	"golang.org/x/net/http2"
 )
@@ -76,51 +75,4 @@ func TestAbortIf(t *testing.T) {
 
 		check(t, cc)
 	})
-}
-
-func TestHeaderByPrefixRemoverModifyRequest(t *testing.T) {
-	withHeader := func(header http.Header) *http.Request {
-		req, err := http.NewRequest(http.MethodGet, "http://example", nil) //nolint:gocritic // This is header test.
-		if err != nil {
-			t.Fatal(err)
-		}
-		req.Header = header
-		return req
-	}
-
-	tests := []struct {
-		name     string
-		prefix   string
-		req      *http.Request
-		expected http.Header
-	}{
-		{
-			name:   "smoke",
-			prefix: http.CanonicalHeaderKey("RemoveMe"),
-			req: withHeader(http.Header{
-				http.CanonicalHeaderKey("RemoveMeByPrefix"): nil,
-				http.CanonicalHeaderKey("RemoveMeBy"):       nil,
-				http.CanonicalHeaderKey("RemoveMe"):         nil,
-				http.CanonicalHeaderKey("DontRemoveMe"):     nil,
-			}),
-			expected: http.Header{
-				http.CanonicalHeaderKey("DontRemoveMe"): nil,
-			},
-		},
-	}
-
-	for i := range tests {
-		tc := &tests[i]
-		t.Run(tc.name, func(t *testing.T) {
-			mod := newHeaderRemover(tc.prefix)
-			req := tc.req
-			err := mod.ModifyRequest(req)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if diff := cmp.Diff(req.Header, tc.expected); diff != "" {
-				t.Fatal(diff)
-			}
-		})
-	}
 }
