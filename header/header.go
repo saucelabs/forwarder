71cde6b9496e41e7dee1ddfaa14384873c50f4e6
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-03-27 14:46:00 +0200

    header: a package to add and remove headers
    The following syntax (and actions) is supported:
    - "<name>: <value>" to add a header,
    - "<name>;" to set a header to empty,
    - "-<name>" to remove a header,
    - "-<name>*" to remove a header by prefix.
    
    This syntax is an extension of the curl -H, --header flag syntax,
    extended with the ability to remove the headers.
    
    This approach has advantages over a flag per action approach, like one found in ngrok.
    
        --request-header-add strings       header key:value to add to request
        --request-header-remove strings    header field to remove from request if present
        --response-header-add strings      header key:value to add to response
        --response-header-remove strings   header field to remove from response if present
    
    With this approach the command help can be simpler and compatible with curl.
    Also, we control the order of execution.
    We can mix and match additions and deletions from different sources.

---
+++ header/header.go
@@ -0,0 +1,112 @@
+// Copyright 2023 Sauce Labs Inc. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package header
+
+import (
+	"errors"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+type Action int
+
+const (
+	Remove Action = iota
+	RemoveByPrefix
+	Empty
+	Add
+)
+
+type Header struct {
+	Name   string
+	Action Action
+	Value  *string
+}
+
+var (
+	headerNameRegex = regexp.MustCompile(`^[A-Za-z0-9-]+$`)
+	headerLineRegex = regexp.MustCompile(`^([A-Za-z0-9-]+):\s*(.*)\r?\n?$`)
+)
+
+// ParseHeader supports the following syntax:
+// - "<name>: <value>" to add a header,
+// - "<name>;" to set a header to empty,
+// - "-<name>" to remove a header,
+// - "-<name>*" to remove a header by prefix.
+func ParseHeader(val string) (Header, error) {
+	var h Header
+
+	if strings.HasPrefix(val, "-") { //nolint:nestif // the branches are very similar
+		if strings.HasSuffix(val, "*") {
+			h.Name = val[1 : len(val)-1]
+			h.Action = RemoveByPrefix
+		} else {
+			h.Name = val[1:]
+			h.Action = Remove
+		}
+	} else {
+		if strings.HasSuffix(val, ";") {
+			h.Name = val[0 : len(val)-1]
+			h.Action = Empty
+		} else {
+			if m := headerLineRegex.FindStringSubmatch(val); m != nil {
+				h.Name = m[1]
+				h.Value = &m[2]
+				h.Action = Add
+			} else {
+				return Header{}, errors.New("invalid header value")
+			}
+		}
+	}
+
+	if !headerNameRegex.MatchString(h.Name) {
+		return Header{}, errors.New("invalid header name")
+	}
+
+	return h, nil
+}
+
+func (h *Header) Apply(hh http.Header) {
+	switch h.Action {
+	case Remove:
+		hh.Del(h.Name)
+	case RemoveByPrefix:
+		removeHeadersByPrefix(hh, h.Name)
+	case Empty:
+		hh.Set(h.Name, "")
+	case Add:
+		hh.Add(h.Name, *h.Value)
+	}
+}
+
+func removeHeadersByPrefix(h http.Header, prefix string) {
+	for k := range h {
+		if len(k) < len(prefix) {
+			continue
+		}
+		if strings.EqualFold(k[0:len(prefix)], prefix) {
+			h.Del(k)
+		}
+	}
+}
+
+type Headers []Header
+
+func (s Headers) ModifyRequest(req *http.Request) error {
+	for _, h := range s {
+		h.Apply(req.Header)
+	}
+	return nil
+}
+
+func (s Headers) ModifyResponse(res *http.Response) error {
+	for _, h := range s {
+		h.Apply(res.Header)
+	}
+	return nil
+}
---
+++ header/header_test.go
@@ -0,0 +1,162 @@
+// Copyright 2023 Sauce Labs Inc. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package header
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestParseHeader(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected Header
+	}{
+		{
+			input: "-RemoveMe",
+			expected: Header{
+				Name:   "RemoveMe",
+				Action: Remove,
+			},
+		},
+		{
+			input: "-RemoveMeByPrefix*",
+			expected: Header{
+				Name:   "RemoveMeByPrefix",
+				Action: RemoveByPrefix,
+			},
+		},
+		{
+			input: "EmptyMe;",
+			expected: Header{
+				Name:   "EmptyMe",
+				Action: Empty,
+			},
+		},
+		{
+			input: "AddMe:value",
+			expected: Header{
+				Name:   "AddMe",
+				Action: Add,
+				Value:  &([]string{"value"})[0],
+			},
+		},
+		{
+			input: "AddMe: value",
+			expected: Header{
+				Name:   "AddMe",
+				Action: Add,
+				Value:  &([]string{"value"})[0],
+			},
+		},
+		{
+			input: "AddMe: value: value",
+			expected: Header{
+				Name:   "AddMe",
+				Action: Add,
+				Value:  &([]string{"value: value"})[0],
+			},
+		},
+		{
+			input: `AddMe: value
+`,
+			expected: Header{
+				Name:   "AddMe",
+				Action: Add,
+				Value:  &([]string{"value"})[0],
+			},
+		},
+	}
+	for i := range tests {
+		tc := &tests[i]
+		t.Run(tc.input, func(t *testing.T) {
+			got, err := ParseHeader(tc.input)
+			if err != nil {
+				t.Errorf("ParseHeader() error = %v", err)
+			}
+			if diff := cmp.Diff(got, tc.expected); diff != "" {
+				t.Errorf("ParseHeader() diff = %v", diff)
+			}
+		})
+	}
+}
+
+func TestParseHeaderError(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		err   error
+	}{
+		{
+			name:  "empty",
+			input: "",
+		},
+		{
+			name:  "remove invalid name",
+			input: "-(@Me)",
+		},
+		{
+			name:  "add invalid name",
+			input: "@Me: value",
+		},
+		{
+			name: "add invalid value",
+			input: `AddMe: value
+value2`,
+		},
+	}
+
+	for i := range tests {
+		tc := &tests[i]
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := ParseHeader(tc.input)
+			t.Log(err)
+			if err == nil {
+				t.Errorf("ParseHeader() error = %v", err)
+			}
+		})
+	}
+}
+
+func TestRemoveHeadersByPrefix(t *testing.T) {
+	tests := []struct {
+		name     string
+		prefix   string
+		header   http.Header
+		expected http.Header
+	}{
+		{
+			name:   "smoke",
+			prefix: http.CanonicalHeaderKey("RemoveMe"),
+			header: http.Header{
+				http.CanonicalHeaderKey("Remo"):             nil,
+				http.CanonicalHeaderKey("RemoveMeByPrefix"): nil,
+				http.CanonicalHeaderKey("RemoveMeBy"):       nil,
+				http.CanonicalHeaderKey("RemoveMe"):         nil,
+				http.CanonicalHeaderKey("DontRemoveMe"):     nil,
+			},
+			expected: http.Header{
+				http.CanonicalHeaderKey("Remo"):         nil,
+				http.CanonicalHeaderKey("DontRemoveMe"): nil,
+			},
+		},
+	}
+
+	for i := range tests {
+		tc := &tests[i]
+		t.Run(tc.name, func(t *testing.T) {
+			h := tc.header.Clone()
+			removeHeadersByPrefix(h, tc.prefix)
+
+			if diff := cmp.Diff(h, tc.expected); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	}
+}
