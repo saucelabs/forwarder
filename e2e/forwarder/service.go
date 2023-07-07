ae817e35a11e4f71dcea31f086e2d7b74c2512c0
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-07-07 12:18:04 +0200

    e2e: move run/setups service and opts to forwarder

--- e2e/forwarder/service.go
+++ e2e/forwarder/service.go
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-package setups
+package forwarder
 
 import (
 	"github.com/saucelabs/forwarder/e2e/compose"
