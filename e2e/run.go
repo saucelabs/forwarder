6a6391f0d93c93adca55a8415172e90e79beda58
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-07-10 14:47:01 +0200

    e2e: extract run into e2e

--- e2e/run.go
+++ e2e/run.go
@@ -11,20 +11,22 @@ import (
 	"log"
 	"regexp"
 
-	"github.com/saucelabs/forwarder/e2e/run/setups"
-	e2esetup "github.com/saucelabs/forwarder/e2e/setup"
+	"github.com/saucelabs/forwarder/e2e/setup"
 )
 
-var (
-	setup = flag.String("setup", "", "Only run setups matching this regexp")
-	debug = flag.Bool("debug", false, "Enables debug logs and preserves containers after running, this will run only the first matching setup")
-)
+var args = struct {
+	setup *string
+	debug *bool
+}{
+	setup: flag.String("setup", "", "Only run setups matching this regexp"),
+	debug: flag.Bool("debug", false, "Enables debug logs and preserves containers after running, this will run only the first matching setup"),
+}
 
 func setupRegexp() (*regexp.Regexp, error) {
-	if *setup == "" {
+	if *args.setup == "" {
 		return nil, nil //nolint:nilnil // this is intentional
 	}
-	return regexp.Compile(*setup)
+	return regexp.Compile(*args.setup)
 }
 
 func main() {
@@ -36,20 +38,20 @@ func main() {
 		log.Fatalf("invalid setup regexp: %v", err)
 	}
 
-	runner := e2esetup.Runner{
-		Setups:      setups.All(),
+	runner := setup.Runner{
+		Setups:      AllSetups(),
 		SetupRegexp: r,
-		Decorate: func(s *e2esetup.Setup) {
+		Decorate: func(s *setup.Setup) {
 			log.Printf("running setup %s", s.Name)
 
-			if *debug {
+			if *args.debug {
 				for _, srv := range s.Compose.Services {
 					srv.Environment["FORWARDER_LOG_LEVEL"] = "debug"
 					srv.Environment["FORWARDER_LOG_HTTP"] = "headers"
 				}
 			}
 		},
-		Debug: *debug,
+		Debug: *args.debug,
 	}
 
 	if err := runner.Run(); err != nil {
