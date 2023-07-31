7e493e0ed4276c8036f96a0aeb83e83a5b395603
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-07-31 12:25:42 +0200

    readurl: add support for reading data scheme
    Reading data scheme is restricted to base64 only.

---
+++ readurl_test.go
@@ -0,0 +1,59 @@
+// Copyright 2023 Sauce Labs Inc. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package forwarder
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestReadURLData(t *testing.T) {
+	tests := []struct {
+		decoded, encoded string
+	}{
+		// RFC 3548 examples
+		{"\x14\xfb\x9c\x03\xd9\x7e", "FPucA9l+"},
+		{"\x14\xfb\x9c\x03\xd9", "FPucA9k="},
+		{"\x14\xfb\x9c\x03", "FPucAw=="},
+
+		// RFC 4648 examples
+		{"", ""},
+		{"f", "Zg=="},
+		{"fo", "Zm8="},
+		{"foo", "Zm9v"},
+		{"foob", "Zm9vYg=="},
+		{"fooba", "Zm9vYmE="},
+		{"foobar", "Zm9vYmFy"},
+
+		// Wikipedia examples
+		{"sure.", "c3VyZS4="},
+		{"sure", "c3VyZQ=="},
+		{"sur", "c3Vy"},
+		{"su", "c3U="},
+		{"leasure.", "bGVhc3VyZS4="},
+		{"easure.", "ZWFzdXJlLg=="},
+		{"asure.", "YXN1cmUu"},
+		{"sure.", "c3VyZS4="},
+	}
+
+	for i := range tests {
+		tc := tests[i]
+		t.Run(tc.encoded, func(t *testing.T) {
+			u := url.URL{
+				Scheme: "data",
+				Opaque: "//base64," + tc.encoded,
+			}
+			b, err := ReadURLString(&u, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if b != tc.decoded {
+				t.Fatalf("expected %q, got %q", tc.decoded, b)
+			}
+		})
+	}
+}
