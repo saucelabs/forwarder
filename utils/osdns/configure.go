ae3b822341301ab9b47c8015caf8b33f27a39781
Hubert Grochowski <hubert.grochowski@saucelabs.com>
2023-08-07 08:14:54 +0200

    utils/osdns: allow overridding os dns config
    Use net package internal functions to configure custom dns options.

---
+++ utils/osdns/configure.go
@@ -0,0 +1,43 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found at https://github.com/golang/go/blob/-/LICENSE.
+
+package osdns
+
+import (
+	"fmt"
+)
+
+func Configure(cfg *Config) error {
+	// Initialize the resolverConfig.
+	getSystemDNSConfig()
+
+	return configure(cfg)
+}
+
+func configure(cfg *Config) error {
+	resolvConf.acquireSema()
+	defer resolvConf.releaseSema()
+
+	procDNSCfg := resolvConf.dnsConfig.Load()
+	if procDNSCfg == nil {
+		return fmt.Errorf("failed to get system DNS config")
+	}
+	if procDNSCfg.err != nil {
+		return fmt.Errorf("failed to get system DNS config: %w", procDNSCfg.err)
+	}
+
+	procDNSCfg.servers = make([]string, len(cfg.Servers))
+	for i := range cfg.Servers {
+		procDNSCfg.servers[i] = cfg.Servers[i].String()
+	}
+	procDNSCfg.timeout = cfg.Timeout
+	procDNSCfg.rotate = cfg.RoundRobin
+
+	// Disable config reload from system dns config file (/etc/resolv.conf).
+	procDNSCfg.noReload = true
+
+	resolvConf.dnsConfig.Store(procDNSCfg)
+
+	return nil
+}
---
+++ utils/osdns/resolvconf.go
@@ -0,0 +1,40 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found at https://github.com/golang/go/blob/-/LICENSE.
+
+package osdns
+
+import (
+	_ "net" // for go:linkname
+	"sync"
+	"sync/atomic"
+	"time"
+	_ "unsafe" // for go:linkname
+)
+
+// A resolverConfig is a 1:1 copy of net.resolverConfig struct.
+// Source: https://github.com/golang/go/blob/-/src/net/dnsclient_unix.go
+// Do not modify.
+//
+//nolint:unused // used by go:linkname
+type resolverConfig struct {
+	initOnce sync.Once // guards init of resolverConfig
+
+	// ch is used as a semaphore that only allows one lookup at a
+	// time to recheck resolv.conf.
+	ch          chan struct{} // guards lastChecked and modTime
+	lastChecked time.Time     // last time resolv.conf was checked
+
+	dnsConfig atomic.Pointer[dnsConfig] // parsed resolv.conf structure used in lookups
+}
+
+func (conf *resolverConfig) acquireSema() {
+	conf.ch <- struct{}{}
+}
+
+func (conf *resolverConfig) releaseSema() {
+	<-conf.ch
+}
+
+//go:linkname resolvConf net.resolvConf
+var resolvConf resolverConfig //nolint:gochecknoglobals // used by go:linkname
