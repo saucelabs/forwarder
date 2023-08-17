4bd534a635c4ec76abfa3ae7e546bc6ffa81dca2
Hubert Grochowski <hubert.grochowski@saucelabs.com>
2023-08-17 13:20:27 +0200

    regex: implement RuleSet
    The RuleSet is a set of regex rules.
    It consist of include and exclude rules.
    The Match method answers the question if given string matches at least one of the include rules
    and does not match the exclude rules.
    Exclude rules takes precedens over the include rules.

---
+++ regex/ruleset.go
@@ -0,0 +1,53 @@
+// Copyright 2023 Sauce Labs Inc. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package regex
+
+import (
+	"regexp"
+	"strings"
+)
+
+type RuleSet struct {
+	include *regexp.Regexp
+	exclude *regexp.Regexp
+}
+
+// NewRuleSet returns the RuleSet with given include and exclude rules.
+// When only the exclude rules are specified, the include rule is set to match everything.
+func NewRuleSet(include, exclude []*regexp.Regexp) *RuleSet {
+	if len(include) == 0 && len(exclude) != 0 {
+		include = []*regexp.Regexp{regexp.MustCompile(".*")}
+	}
+
+	build := func(rules []*regexp.Regexp) *regexp.Regexp {
+		var regex strings.Builder
+		for i := range rules {
+			if i > 0 {
+				regex.WriteString("|")
+			}
+			regex.WriteString(rules[i].String())
+		}
+		if s := regex.String(); s != "" {
+			return regexp.MustCompile(s)
+		}
+		return nil
+	}
+
+	return &RuleSet{
+		include: build(include),
+		exclude: build(exclude),
+	}
+}
+
+// Match returns true if the given string matches at least one of the include rules
+// and does not match the exclude rules.
+func (r *RuleSet) Match(s string) bool {
+	if r.exclude != nil && r.exclude.MatchString(s) {
+		return false
+	}
+	return r.include != nil && r.include.MatchString(s)
+}
