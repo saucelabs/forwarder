43f78743b9456286c3bb1094cf509887c1250e73
Hubert Grochowski <hubert.grochowski@saucelabs.com>
2023-09-06 10:34:25 +0200

    e2e/tests: rework request and response header flag tests to use `/headers/` endpoint

--- e2e/tests/flag_test.go
+++ e2e/tests/flag_test.go
@@ -35,34 +35,25 @@ func TestFlagProxyLocalhost(t *testing.T) {
 func TestFlagHeader(t *testing.T) {
 	c := newClient(t, httpbin)
 
-	c.GET("/header/test-add/test-value").
-		ExpectStatus(http.StatusOK)
+	c.GET("/headers/").ExpectStatus(http.StatusOK).ExpectHeader("test-add", "test-value")
 
-	c.GET("/header/test-empty/", setHeader("test-empty", "not-empty")).
-		ExpectStatus(http.StatusOK)
+	c.GET("/headers/", setHeader("test-empty", "not-empty")).ExpectStatus(http.StatusOK).ExpectHeader("test-empty", "")
 
-	c.GET("/header/test-rm/value-1", setHeader("test-rm", "value-1")).
-		ExpectStatus(http.StatusNotFound)
+	c.GET("/headers/", setHeader("test-rm", "value-1")).ExpectStatus(http.StatusOK).ExpectHeader("test-rm", "")
 
-	c.GET("/header/rm-prefix/value-2", setHeader("rm-prefix", "value-2")).
-		ExpectStatus(http.StatusNotFound)
+	c.GET("/headers/", setHeader("rm-prefix", "value-2")).ExpectStatus(http.StatusOK).ExpectHeader("rm-prefix", "")
 }
 
 func TestFlagResponseHeader(t *testing.T) {
 	c := newClient(t, httpbin)
 
-	c.GET("/status/200").ExpectStatus(http.StatusOK).
-		ExpectHeader("test-resp-add", "test-resp-value")
+	c.GET("/status/200").ExpectStatus(http.StatusOK).ExpectHeader("test-resp-add", "test-resp-value")
 
-	c.GET("/header/test-resp-empty/not-empty", setHeader("test-resp-empty", "not-empty")).
-		ExpectStatus(http.StatusOK).
-		ExpectHeader("test-resp-empty", "")
+	c.GET("/headers/", setHeader("test-resp-empty", "not-empty")).ExpectStatus(http.StatusOK).ExpectHeader("test-resp-empty", "")
 
-	c.GET("/header/test-resp-rm/value-3", setHeader("test-resp-rm", "value-3")).
-		ExpectStatus(http.StatusOK).ExpectHeader("test-resp-rm", "")
+	c.GET("/headers/", setHeader("test-resp-rm", "value-3")).ExpectStatus(http.StatusOK).ExpectHeader("test-resp-rm", "")
 
-	c.GET("/header/resp-rm-prefix/value-4", setHeader("resp-rm-prefix", "value-4")).
-		ExpectStatus(http.StatusOK).ExpectHeader("resp-rm-prefix", "")
+	c.GET("/headers/", setHeader("resp-rm-prefix", "value-4")).ExpectStatus(http.StatusOK).ExpectHeader("resp-rm-prefix", "")
 }
 
 func setHeader(key, value string) func(r *http.Request) {
