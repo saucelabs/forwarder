627add087e1f4f1964e67e8e49107e9ae42c5e2d
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-09-11 14:35:16 +0200

    log/martianlog: export martian log setting

---
+++ log/martianlog/martianlog.go
@@ -0,0 +1,16 @@
+// Copyright 2023 Sauce Labs Inc. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package martianlog
+
+import (
+	martianlog "github.com/saucelabs/forwarder/internal/martian/log"
+	"github.com/saucelabs/forwarder/log"
+)
+
+func SetLogger(l log.Logger) {
+	martianlog.SetLogger(l)
+}
