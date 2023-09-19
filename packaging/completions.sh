d5620c13645b753544bf07685b0f470f7dadcb04
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-09-19 15:54:52 +0200

    packaging: add powershell to completions.sh
    Fixes #424

--- packaging/completions.sh
+++ packaging/completions.sh
@@ -5,6 +5,6 @@ set -e
 rm -rf completions
 mkdir completions
 
-for sh in bash zsh fish; do
+for sh in bash zsh powershell fish; do
   go run ./cmd/forwarder completion "$sh" >"completions/forwarder.$sh"
 done
