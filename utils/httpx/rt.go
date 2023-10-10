26a961f60233bc8eb0198008ab44b99d8feda137
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-10-10 13:44:14 +0200

    utlis/httpx: add RoundTripperFunc

---
+++ utils/httpx/rt.go
@@ -0,0 +1,18 @@
+// Copyright 2023 Sauce Labs Inc. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package httpx
+
+import (
+	"net/http"
+)
+
+// RoundTripperFunc is an adapter to allow the use of ordinary functions as HTTP round trippers.
+type RoundTripperFunc func(*http.Request) (*http.Response, error)
+
+func (f RoundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) {
+	return f(r)
+}
