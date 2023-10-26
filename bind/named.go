49247d32faed36dbede64c0392f105df0f0d27e6
Alex Harford <alex.harford@saucelabs.com>
2023-10-26 10:33:55 -0700

    bind: configure multiple services with single --log-http [a|b|c:]<mode> flag
    Extract from Forwarder help message
    
        --log-http [api|proxy:]<none|short-url|url|headers|body|errors>,...  (env FORWARDER_LOG_HTTP)
            HTTP request and response logging mode. Setting this to none disables logging. The short-url mode logs
            [scheme://]host[/path] instead of the full URL. The error mode logs request line and headers if status code is
            greater than or equal to 500.
    
    Co-authored-by: Michal Jan Matczuk <michal.matczuk@saucelabs.com>

---
+++ bind/named.go
@@ -0,0 +1,24 @@
+// Copyright 2023 Sauce Labs Inc. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package bind
+
+import (
+	"fmt"
+)
+
+type NamedParam[T fmt.Stringer] struct {
+	Name  string
+	Param *T
+}
+
+func (p NamedParam[T]) String() string {
+	if p.Name == "" {
+		return (*p.Param).String()
+	}
+
+	return p.Name + ":" + (*p.Param).String()
+}
---
+++ httplog/httplog_test.go
@@ -0,0 +1,62 @@
+// Copyright 2023 Sauce Labs Inc. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package httplog
+
+import (
+	"testing"
+)
+
+func TestSplitNameMode(t *testing.T) {
+	tests := []struct {
+		val  string
+		name string
+		mode Mode
+	}{
+		{
+			val:  "api:none",
+			name: "api",
+			mode: None,
+		},
+		{
+			val:  "errors",
+			name: "",
+			mode: Errors,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.val, func(t *testing.T) {
+			n, m, err := SplitNameMode(tc.val)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if n != tc.name {
+				t.Errorf("expected name %q, got %q", tc.name, n)
+			}
+			if m != tc.mode {
+				t.Errorf("expected mode %q, got %q", tc.mode, m)
+			}
+		})
+	}
+}
+
+func TestSplitNameModeError(t *testing.T) {
+	tests := []string{
+		"api:invalid",
+		"invalid",
+	}
+
+	for _, tc := range tests {
+		t.Run(tc, func(t *testing.T) {
+			_, _, err := SplitNameMode(tc)
+			if err == nil {
+				t.Fatalf("expected error, got nil")
+			}
+			t.Log(err)
+		})
+	}
+}
