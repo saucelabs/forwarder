00e17acb385af757c1ac25b4b02333b8a1039c5b
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-11-14 12:14:12 +0100

    log/stdlog: refactor exported fields to functional options
    This reduces the scope of the fields and avoids data races.

---
+++ log/stdlog/opts.go
@@ -0,0 +1,14 @@
+// Copyright 2023 Sauce Labs Inc. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package stdlog
+
+// WithDecorate allows to a function that modifies the log message before it is written.
+func WithDecorate(f func(string) string) Option {
+	return func(l *Logger) {
+		l.decorate = f
+	}
+}
