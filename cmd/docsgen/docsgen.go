f3e824983401de9e852487a8b6988d86fe9dd63b
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-11-21 16:18:11 +0100

    cmd/docsgen: add script to generate cli command reference

---
+++ cmd/docsgen/docsgen.go
@@ -0,0 +1,185 @@
+// Copyright 2023 Sauce Labs Inc., all rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package main
+
+//go:generate go run .
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	"github.com/saucelabs/forwarder/command/forwarder"
+	"github.com/saucelabs/forwarder/utils/cobrautil"
+	"github.com/saucelabs/forwarder/utils/cobrautil/templates"
+	"github.com/spf13/cobra"
+)
+
+func gitRoot() (string, error) {
+	var b bytes.Buffer
+
+	c := exec.Command("git", "rev-parse", "--show-toplevel")
+	c.Stdout = &b
+	if err := c.Run(); err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(b.String()), nil
+}
+
+var docsDir, cliDir, cfgDir string
+
+func init() {
+	r, err := gitRoot()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	docsDir = path.Join(r, "docs", "content")
+	cliDir = path.Join(docsDir, "cli")
+	cfgDir = path.Join(docsDir, "config")
+}
+
+func main() {
+	for _, dir := range []string{docsDir, cliDir, cfgDir} {
+		if err := os.MkdirAll(dir, 0o700); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	cg := forwarder.CommandGroups()
+	cg.Add(&cobra.Command{
+		Use: "forwarder",
+	})
+	if err := writeCommandIndex(cg); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := writeCommandDoc(forwarder.Command()); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := writeDefaultConfig(forwarder.Command()); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var cmdIndex map[string]int
+
+func writeCommandIndex(cg templates.CommandGroups) error {
+	cmdIndex = make(map[string]int)
+
+	f, err := os.Create(path.Join(cliDir, "toc.md"))
+	if err != nil {
+		return err
+	}
+
+	var items []string
+	for _, g := range cg {
+		for _, cmd := range g.Commands {
+			walkCommand(cmd, func(cmd *cobra.Command) {
+				if cmd.IsAvailableCommand() && cmd.Flags().HasFlags() {
+					items = append(items, fmt.Sprintf("- [%s](%s) - %s",
+						cobrautil.FullCommandName(cmd),
+						fileName(cmd, ".md"),
+						cmd.Short),
+					)
+					cmdIndex[cobrautil.FullCommandName(cmd)] = len(items)
+				}
+			})
+		}
+	}
+
+	fmt.Fprintf(f, "---\nbookHidden: true\n---\n\n")
+
+	fmt.Fprint(f, "# Forwarder CLI\n\n")
+	for _, item := range items {
+		fmt.Fprintf(f, "%s\n", item)
+	}
+
+	return f.Close()
+}
+
+const cliWeight = 100
+
+func writeCommandDoc(cmd *cobra.Command) error {
+	for _, cmd := range cmd.Commands() {
+		if err := writeCommandDoc(cmd); err != nil {
+			return err
+		}
+	}
+
+	if cmd.IsAvailableCommand() && cmd.Flags().HasFlags() {
+		f, err := os.Create(path.Join(cliDir, fileName(cmd, ".md")))
+		if err != nil {
+			return err
+		}
+
+		if cmd.Long != "" {
+			cmd.Long += "\n\n" + configFileNote(cmd)
+		} else if cmd.Short != "" {
+			cmd.Short += "\n\n" + configFileNote(cmd)
+		}
+
+		fmt.Fprintf(f, "---\n")
+		fmt.Fprintf(f, "title: %s\n", cobrautil.FullCommandName(cmd))
+		if idx, ok := cmdIndex[cobrautil.FullCommandName(cmd)]; ok {
+			fmt.Fprintf(f, "weight: %d\n", cliWeight+idx)
+		}
+		fmt.Fprintf(f, "---\n\n")
+
+		cobrautil.WriteMarkdownDoc(f, forwarder.FlagGroups(), forwarder.EnvPrefix, cmd)
+
+		return f.Close()
+	}
+
+	return nil
+}
+
+func configFileNote(cmd *cobra.Command) string {
+	return fmt.Sprintf(
+		"**Note:** You can also specify the options as YAML, JSON or TOML file using `--config-file` flag.\n"+
+			"You can generate a config file by running `%s` command.\n",
+		cobrautil.FullCommandName(cmd)+" config-file",
+	)
+}
+
+func writeDefaultConfig(cmd *cobra.Command) error {
+	for _, cmd := range cmd.Commands() {
+		if err := writeDefaultConfig(cmd); err != nil {
+			return err
+		}
+	}
+
+	if cmd.IsAvailableCommand() && cmd.Flags().HasFlags() {
+		f, err := os.Create(path.Join(cfgDir, fileName(cmd, ".yaml")))
+		if err != nil {
+			return err
+		}
+
+		cobrautil.WriteConfigFile(f, forwarder.FlagGroups(), forwarder.ConfigFileFlagName, cmd.Flags())
+
+		return f.Close()
+	}
+
+	return nil
+}
+
+func fileName(cmd *cobra.Command, ext string) string {
+	return strings.ReplaceAll(cobrautil.FullCommandName(cmd), " ", "_") + ext
+}
+
+func walkCommand(cmd *cobra.Command, f func(cmd *cobra.Command)) {
+	f(cmd)
+	for _, cmd := range cmd.Commands() {
+		walkCommand(cmd, f)
+	}
+}
