fab49d456cea7ce1f659b95162650b66e0cf568a
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-11-27 16:01:20 +0100

    chore(cmd/docsgen): extract write functions to separate file

--- cmd/docsgen/docsgen.go
+++ cmd/docsgen/docsgen.go
@@ -8,15 +8,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"path"
-	"strings"
 
 	"github.com/saucelabs/forwarder/command/forwarder"
-	"github.com/saucelabs/forwarder/utils/cobrautil"
-	"github.com/saucelabs/forwarder/utils/cobrautil/templates"
 	"github.com/spf13/cobra"
 )
 
@@ -52,117 +48,5 @@ func main() {
 
 	if err := writeDefaultConfig(forwarder.Command()); err != nil {
 		log.Fatal(err)
-	}
-}
-
-var cmdIndex map[string]int
-
-func writeCommandIndex(cg templates.CommandGroups) error {
-	cmdIndex = make(map[string]int)
-
-	f, err := os.Create(path.Join(cliDir, "toc.md"))
-	if err != nil {
-		return err
-	}
-
-	var items []string
-	for _, g := range cg {
-		for _, cmd := range g.Commands {
-			walkCommand(cmd, func(cmd *cobra.Command) {
-				if cmd.IsAvailableCommand() && cmd.Flags().HasFlags() {
-					items = append(items, fmt.Sprintf("- [%s](%s) - %s",
-						cobrautil.FullCommandName(cmd),
-						fileName(cmd, ".md"),
-						cmd.Short),
-					)
-					cmdIndex[cobrautil.FullCommandName(cmd)] = len(items)
-				}
-			})
-		}
-	}
-
-	fmt.Fprintf(f, "---\nbookHidden: true\n---\n\n")
-
-	fmt.Fprint(f, "# Forwarder CLI\n\n")
-	for _, item := range items {
-		fmt.Fprintf(f, "%s\n", item)
-	}
-
-	return f.Close()
-}
-
-const cliWeight = 100
-
-func writeCommandDoc(cmd *cobra.Command) error {
-	for _, cmd := range cmd.Commands() {
-		if err := writeCommandDoc(cmd); err != nil {
-			return err
-		}
-	}
-
-	if cmd.IsAvailableCommand() && cmd.Flags().HasFlags() {
-		f, err := os.Create(path.Join(cliDir, fileName(cmd, ".md")))
-		if err != nil {
-			return err
-		}
-
-		if cmd.Long != "" {
-			cmd.Long += "\n\n" + configFileNote(cmd)
-		} else if cmd.Short != "" {
-			cmd.Short += "\n\n" + configFileNote(cmd)
-		}
-
-		fmt.Fprintf(f, "---\n")
-		fmt.Fprintf(f, "title: %s\n", cobrautil.FullCommandName(cmd))
-		if idx, ok := cmdIndex[cobrautil.FullCommandName(cmd)]; ok {
-			fmt.Fprintf(f, "weight: %d\n", cliWeight+idx)
-		}
-		fmt.Fprintf(f, "---\n\n")
-
-		cobrautil.WriteMarkdownDoc(f, forwarder.FlagGroups(), forwarder.EnvPrefix, cmd)
-
-		return f.Close()
-	}
-
-	return nil
-}
-
-func configFileNote(cmd *cobra.Command) string {
-	return fmt.Sprintf(
-		"**Note:** You can also specify the options as YAML, JSON or TOML file using `--config-file` flag.\n"+
-			"You can generate a config file by running `%s` command.\n",
-		cobrautil.FullCommandName(cmd)+" config-file",
-	)
-}
-
-func writeDefaultConfig(cmd *cobra.Command) error {
-	for _, cmd := range cmd.Commands() {
-		if err := writeDefaultConfig(cmd); err != nil {
-			return err
-		}
-	}
-
-	if cmd.IsAvailableCommand() && cmd.Flags().HasFlags() {
-		f, err := os.Create(path.Join(cfgDir, fileName(cmd, ".yaml")))
-		if err != nil {
-			return err
-		}
-
-		cobrautil.WriteConfigFile(f, forwarder.FlagGroups(), forwarder.ConfigFileFlagName, cmd.Flags())
-
-		return f.Close()
-	}
-
-	return nil
-}
-
-func fileName(cmd *cobra.Command, ext string) string {
-	return strings.ReplaceAll(cobrautil.FullCommandName(cmd), " ", "_") + ext
-}
-
-func walkCommand(cmd *cobra.Command, f func(cmd *cobra.Command)) {
-	f(cmd)
-	for _, cmd := range cmd.Commands() {
-		walkCommand(cmd, f)
 	}
 }
