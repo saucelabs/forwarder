fb2f30914a7154c87380490bfdf0c16e14b451ed
Michał Matczuk <michal.matczuk@saucelabs.com>
2023-11-29 17:44:22 +0100

    packaging(completion): use ps1 extension for powershell

--- packaging/completions.sh
+++ packaging/completions.sh
@@ -8,3 +8,6 @@ mkdir completions
 for sh in bash zsh powershell fish; do
   go run ./cmd/forwarder completion "$sh" >"completions/forwarder.$sh"
 done
+
+# Set powershell extension to ps1.
+mv completions/forwarder.powershell completions/forwarder.ps1
