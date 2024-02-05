3c168c9956fe3adaf5dd3ac05929ccaf33a11de2
Michał Matczuk <michal.matczuk@saucelabs.com>
2024-02-05 14:44:09 +0100

    martian: add proxyConn and streamline Proxy function arguments
    Fixes #524

--- internal/martian/proxy.go
+++ internal/martian/proxy.go
@@ -339,6 +339,13 @@ func (p *Proxy) Serve(l net.Listener) error {
 	}
 }
 
+type proxyConn struct {
+	*Proxy
+	session *Session
+	brw     *bufio.ReadWriter
+	conn    net.Conn
+}
+
 func (p *Proxy) handleLoop(conn net.Conn) {
 	p.connsMu.Lock()
 	p.conns.Add(1)
@@ -349,16 +356,19 @@ func (p *Proxy) handleLoop(conn net.Conn) {
 		return
 	}
 
-	var (
-		brw = bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
-		s   = newSession(conn, brw)
-		ctx = withSession(s)
-	)
+	brw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+
+	pc := &proxyConn{
+		Proxy:   p,
+		session: newSession(conn, brw),
+		brw:     brw,
+		conn:    conn,
+	}
 
 	const maxConsecutiveErrors = 5
 	errorsN := 0
 	for {
-		if err := p.handle(ctx, conn, brw); err != nil {
+		if err := pc.handle(withSession(pc.session)); err != nil {
 			if errors.Is(err, errClose) || isCloseable(err) {
 				log.Debugf(context.TODO(), "closing connection: %v", conn.RemoteAddr())
 				return
@@ -373,7 +383,7 @@ func (p *Proxy) handleLoop(conn net.Conn) {
 			errorsN = 0
 		}
 
-		if s.Hijacked() {
+		if pc.session.Hijacked() {
 			log.Debugf(context.TODO(), "closing connection: %v", conn.RemoteAddr())
 			return
 		}
@@ -394,12 +404,12 @@ func (p *Proxy) readHeaderTimeout() time.Duration {
 	return p.ReadTimeout
 }
 
-func (p *Proxy) readRequest(ctx *Context, conn net.Conn, brw *bufio.ReadWriter) (*http.Request, error) {
+func (p *proxyConn) readRequest(ctx *Context) (*http.Request, error) {
 	var idleDeadline time.Time // or zero if none
 	if d := p.idleTimeout(); d > 0 {
 		idleDeadline = time.Now().Add(d)
 	}
-	if deadlineErr := conn.SetReadDeadline(idleDeadline); deadlineErr != nil {
+	if deadlineErr := p.conn.SetReadDeadline(idleDeadline); deadlineErr != nil {
 		log.Errorf(context.TODO(), "can't set idle deadline: %v", deadlineErr)
 	}
 
@@ -407,7 +417,7 @@ func (p *Proxy) readRequest(ctx *Context, conn net.Conn, brw *bufio.ReadWriter) 
 	// read the next request. This prevents a ReadHeaderTimeout or
 	// ReadTimeout from starting until the first bytes of the next request
 	// have been received.
-	if _, err := brw.Peek(1); err != nil {
+	if _, err := p.brw.Peek(1); err != nil {
 		return nil, err
 	}
 
@@ -423,11 +433,11 @@ func (p *Proxy) readRequest(ctx *Context, conn net.Conn, brw *bufio.ReadWriter) 
 		wholeReqDeadline = t0.Add(d)
 	}
 
-	if deadlineErr := conn.SetReadDeadline(hdrDeadline); deadlineErr != nil {
+	if deadlineErr := p.conn.SetReadDeadline(hdrDeadline); deadlineErr != nil {
 		log.Errorf(context.TODO(), "can't set read header deadline: %v", deadlineErr)
 	}
 
-	req, err := http.ReadRequest(brw.Reader)
+	req, err := http.ReadRequest(p.brw.Reader)
 	if err != nil {
 		return nil, err
 	}
@@ -436,7 +446,7 @@ func (p *Proxy) readRequest(ctx *Context, conn net.Conn, brw *bufio.ReadWriter) 
 
 	// Adjust the read deadline if necessary.
 	if !hdrDeadline.Equal(wholeReqDeadline) {
-		if deadlineErr := conn.SetReadDeadline(wholeReqDeadline); deadlineErr != nil {
+		if deadlineErr := p.conn.SetReadDeadline(wholeReqDeadline); deadlineErr != nil {
 			log.Errorf(context.TODO(), "can't set read deadline: %v", deadlineErr)
 		}
 	}
@@ -478,7 +488,7 @@ func shouldTerminateTLS(req *http.Request) bool {
 	return b
 }
 
-func (p *Proxy) handleMITM(ctx *Context, req *http.Request, session *Session, brw *bufio.ReadWriter, conn net.Conn) error {
+func (p *proxyConn) handleMITM(ctx *Context, req *http.Request) error {
 	log.Debugf(req.Context(), "mitm: attempting MITM for connection %s", req.Host)
 
 	res := proxyutil.NewResponse(200, nil, req)
@@ -487,16 +497,16 @@ func (p *Proxy) handleMITM(ctx *Context, req *http.Request, session *Session, br
 		log.Errorf(req.Context(), "mitm: error modifying CONNECT response: %v", err)
 		p.warning(res.Header, err)
 	}
-	if session.Hijacked() {
+	if p.session.Hijacked() {
 		log.Debugf(req.Context(), "mitm: connection hijacked by response modifier")
 		return nil
 	}
 
-	if err := p.writeResponse(res, brw, conn); err != nil {
+	if err := p.writeResponse(res); err != nil {
 		return fmt.Errorf("mitm: write CONNECT response: %w", err)
 	}
 
-	b, err := brw.Peek(1)
+	b, err := p.brw.Peek(1)
 	if err != nil {
 		if isClosedConnError(err) {
 			log.Debugf(req.Context(), "mitm: connection closed prematurely: %v", err)
@@ -507,16 +517,16 @@ func (p *Proxy) handleMITM(ctx *Context, req *http.Request, session *Session, br
 	}
 
 	// Drain all of the rest of the buffered data.
-	buf := make([]byte, brw.Reader.Buffered())
-	brw.Read(buf)
+	buf := make([]byte, p.brw.Reader.Buffered())
+	p.brw.Read(buf)
 
 	// 22 is the TLS handshake.
 	// https://tools.ietf.org/html/rfc5246#section-6.2.1
 	if len(b) > 0 && b[0] == 22 {
 		// Prepend the previously read data to be read again by http.ReadRequest.
 		tlsconn := tls.Server(&peekedConn{
-			conn,
-			io.MultiReader(bytes.NewReader(buf), conn),
+			p.conn,
+			io.MultiReader(bytes.NewReader(buf), p.conn),
 		}, p.mitm.TLSForHost(req.Host))
 
 		var hctx context.Context
@@ -544,28 +554,29 @@ func (p *Proxy) handleMITM(ctx *Context, req *http.Request, session *Session, br
 			return p.mitm.H2Config().Proxy(p.closing, tlsconn, req.URL)
 		}
 
-		brw.Writer.Reset(tlsconn)
-		brw.Reader.Reset(tlsconn)
-		return p.handle(ctx, tlsconn, brw)
+		p.brw.Writer.Reset(tlsconn)
+		p.brw.Reader.Reset(tlsconn)
+		p.conn = tlsconn
+		return p.handle(ctx)
 	}
 
 	// Prepend the previously read data to be read again by http.ReadRequest.
-	brw.Reader.Reset(io.MultiReader(bytes.NewReader(buf), conn))
-	return p.handle(ctx, conn, brw)
+	p.brw.Reader.Reset(io.MultiReader(bytes.NewReader(buf), p.conn))
+	return p.handle(ctx)
 }
 
-func (p *Proxy) handleConnectRequest(ctx *Context, req *http.Request, session *Session, brw *bufio.ReadWriter, conn net.Conn) error {
+func (p *proxyConn) handleConnectRequest(ctx *Context, req *http.Request) error {
 	if err := p.reqmod.ModifyRequest(req); err != nil {
 		log.Errorf(req.Context(), "error modifying CONNECT request: %v", err)
 		p.warning(req.Header, err)
 	}
-	if session.Hijacked() {
+	if p.session.Hijacked() {
 		log.Debugf(req.Context(), "connection hijacked by request modifier")
 		return nil
 	}
 
 	if p.shouldMITM(req) {
-		return p.handleMITM(ctx, req, session, brw, conn)
+		return p.handleMITM(ctx, req)
 	}
 
 	log.Debugf(req.Context(), "attempting to establish CONNECT tunnel: %s", req.URL.Host)
@@ -609,7 +620,7 @@ func (p *Proxy) handleConnectRequest(ctx *Context, req *http.Request, session *S
 		log.Errorf(req.Context(), "error modifying CONNECT response: %v", err)
 		p.warning(res.Header, err)
 	}
-	if session.Hijacked() {
+	if p.session.Hijacked() {
 		log.Debugf(req.Context(), "connection hijacked by response modifier")
 		return nil
 	}
@@ -618,7 +629,7 @@ func (p *Proxy) handleConnectRequest(ctx *Context, req *http.Request, session *S
 		if cerr == nil {
 			log.Errorf(req.Context(), "CONNECT rejected with status code: %d", res.StatusCode)
 		}
-		if err := p.writeResponse(res, brw, conn); err != nil {
+		if err := p.writeResponse(res); err != nil {
 			return fmt.Errorf("write CONNECT error response: %w", err)
 		}
 		return nil
@@ -626,14 +637,14 @@ func (p *Proxy) handleConnectRequest(ctx *Context, req *http.Request, session *S
 
 	res.ContentLength = -1
 
-	if err := p.tunnel("CONNECT", res, brw, conn, crw); err != nil {
+	if err := p.tunnel("CONNECT", res, crw); err != nil {
 		log.Errorf(req.Context(), "CONNECT tunnel: %w", err)
 	}
 
 	return errClose
 }
 
-func (p *Proxy) handleUpgradeResponse(res *http.Response, brw *bufio.ReadWriter, conn net.Conn) error {
+func (p *proxyConn) handleUpgradeResponse(res *http.Response) error {
 	resUpType := upgradeType(res.Header)
 
 	uconn, ok := res.Body.(io.ReadWriteCloser)
@@ -644,28 +655,28 @@ func (p *Proxy) handleUpgradeResponse(res *http.Response, brw *bufio.ReadWriter,
 
 	res.Body = nil
 
-	if err := p.tunnel(resUpType, res, brw, conn, uconn); err != nil {
+	if err := p.tunnel(resUpType, res, uconn); err != nil {
 		log.Errorf(res.Request.Context(), "%s tunnel: %w", resUpType, err)
 	}
 
 	return errClose
 }
 
-func (p *Proxy) tunnel(name string, res *http.Response, brw *bufio.ReadWriter, conn net.Conn, crw io.ReadWriteCloser) error {
-	if err := res.Write(brw); err != nil {
+func (p *proxyConn) tunnel(name string, res *http.Response, crw io.ReadWriteCloser) error {
+	if err := res.Write(p.brw); err != nil {
 		return fmt.Errorf("got error while writing response back to client: %w", err)
 	}
-	if err := brw.Flush(); err != nil {
+	if err := p.brw.Flush(); err != nil {
 		return fmt.Errorf("got error while flushing response back to client: %w", err)
 	}
-	if err := drainBuffer(crw, brw.Reader); err != nil {
+	if err := drainBuffer(crw, p.brw.Reader); err != nil {
 		return fmt.Errorf("got error while draining read buffer: %w", err)
 	}
 
 	ctx := res.Request.Context()
 	donec := make(chan bool, 2)
-	go copySync(ctx, "outbound "+name, crw, conn, donec)
-	go copySync(ctx, "inbound "+name, conn, crw, donec)
+	go copySync(ctx, "outbound "+name, crw, p.conn, donec)
+	go copySync(ctx, "inbound "+name, p.conn, crw, donec)
 
 	log.Debugf(ctx, "switched protocols, proxying %s traffic", name)
 	<-donec
@@ -713,13 +724,13 @@ func copySync(ctx context.Context, name string, w io.Writer, r io.Reader, donec 
 	donec <- true
 }
 
-func (p *Proxy) handle(ctx *Context, conn net.Conn, brw *bufio.ReadWriter) error {
-	log.Debugf(context.TODO(), "waiting for request: %v", conn.RemoteAddr())
+func (p *proxyConn) handle(ctx *Context) error {
+	log.Debugf(context.TODO(), "waiting for request: %v", p.conn.RemoteAddr())
 
 	session := ctx.Session()
 	ctx = withSession(session)
 
-	req, err := p.readRequest(ctx, conn, brw)
+	req, err := p.readRequest(ctx)
 	if err != nil {
 		if errors.Is(err, io.EOF) {
 			return errClose
@@ -738,20 +749,20 @@ func (p *Proxy) handle(ctx *Context, conn net.Conn, brw *bufio.ReadWriter) error
 		return errClose
 	}
 
-	if tconn, ok := conn.(*tls.Conn); ok {
+	if tconn, ok := p.conn.(*tls.Conn); ok {
 		session.MarkSecure()
 
 		cs := tconn.ConnectionState()
 		req.TLS = &cs
 	}
 
-	req.RemoteAddr = conn.RemoteAddr().String()
+	req.RemoteAddr = p.conn.RemoteAddr().String()
 	if req.URL.Host == "" {
 		req.URL.Host = req.Host
 	}
 
 	if req.Method == http.MethodConnect {
-		return p.handleConnectRequest(ctx, req, session, brw, conn)
+		return p.handleConnectRequest(ctx, req)
 	}
 
 	if req.URL.Scheme == "" {
@@ -826,10 +837,10 @@ func (p *Proxy) handle(ctx *Context, conn net.Conn, brw *bufio.ReadWriter) error
 
 	// deal with 101 Switching Protocols responses: (WebSocket, h2c, etc)
 	if res.StatusCode == http.StatusSwitchingProtocols {
-		return p.handleUpgradeResponse(res, brw, conn)
+		return p.handleUpgradeResponse(res)
 	}
 
-	if err := p.writeResponse(res, brw, conn); err != nil {
+	if err := p.writeResponse(res); err != nil {
 		if errors.Is(err, errClose) {
 			return errClose
 		}
@@ -845,14 +856,14 @@ func (p *Proxy) handle(ctx *Context, conn net.Conn, brw *bufio.ReadWriter) error
 	return nil
 }
 
-func (p *Proxy) writeResponse(res *http.Response, brw *bufio.ReadWriter, conn net.Conn) error {
+func (p *proxyConn) writeResponse(res *http.Response) error {
 	req := res.Request
 
 	if p.WriteTimeout > 0 {
-		if deadlineErr := conn.SetWriteDeadline(time.Now().Add(p.WriteTimeout)); deadlineErr != nil {
+		if deadlineErr := p.conn.SetWriteDeadline(time.Now().Add(p.WriteTimeout)); deadlineErr != nil {
 			log.Errorf(req.Context(), "can't set write deadline: %v", deadlineErr)
 		}
-		defer conn.SetWriteDeadline(time.Time{})
+		defer p.conn.SetWriteDeadline(time.Time{})
 	}
 
 	if !req.ProtoAtLeast(1, 1) || req.Close || res.Close || p.Closing() {
@@ -865,23 +876,23 @@ func (p *Proxy) writeResponse(res *http.Response, brw *bufio.ReadWriter, conn ne
 		// The http package is misbehaving when writing a HEAD response.
 		// See https://github.com/golang/go/issues/62015 for details.
 		// This works around the issue by writing the response manually.
-		err = writeHeadResponse(brw.Writer, res)
+		err = writeHeadResponse(p.brw.Writer, res)
 	} else {
 		// Add support for Server Sent Events - relay HTTP chunks and flush after each chunk.
 		// This is safe for events that are smaller than the buffer io.Copy uses (32KB).
 		// If the event is larger than the buffer, the event will be split into multiple chunks.
 		if shouldFlush(res) {
-			err = res.Write(flushAfterChunkWriter{brw.Writer})
+			err = res.Write(flushAfterChunkWriter{p.brw.Writer})
 		} else {
-			err = res.Write(brw)
+			err = res.Write(p.brw)
 		}
 	}
 	if err != nil {
-		brw.Flush() // flush any remaining data
+		p.brw.Flush() // flush any remaining data
 		return err
 	}
 
-	if err := brw.Flush(); err != nil {
+	if err := p.brw.Flush(); err != nil {
 		return err
 	}
 
