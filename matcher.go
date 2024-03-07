e011d4ef1dc8881c47ecfeb2721718ecbc4dac00
Hubert Grochowski <hubert.grochowski@saucelabs.com>
2024-03-07 15:15:44 +0100

    matcher: make matcher interface
    The Matcher interface will replace RegexpMatcher in forwarder's config.
    This allows specifying custom match logic.
    
    This patch also adds a MatchFunc - helper for satisfying Matcher interface with functions.

---
+++ matcher.go
@@ -0,0 +1,17 @@
+// Copyright 2022-2024 Sauce Labs Inc., all rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package forwarder
+
+type Matcher interface {
+	Match(string) bool
+}
+
+type MatchFunc func(string) bool
+
+func (m MatchFunc) Match(s string) bool {
+	return m(s)
+}
