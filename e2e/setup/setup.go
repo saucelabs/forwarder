63a348000d337e16475bf8dcfb0582f6ff728440
Michał Matczuk <michal.matczuk@saucelabs.com>
2024-04-03 13:04:08 +0200

    e2e/setup: run tests in parallel

--- e2e/setup/setup.go
+++ e2e/setup/setup.go
@@ -7,12 +7,17 @@
 package setup
 
 import (
+	"context"
 	"fmt"
+	"math/rand"
 	"os"
 	"regexp"
+	"slices"
 	"time"
 
 	"github.com/saucelabs/forwarder/utils/compose"
+	"go.uber.org/multierr"
+	"golang.org/x/sync/errgroup"
 )
 
 const TestServiceName = "test"
@@ -28,11 +33,29 @@ type Runner struct {
 	SetupRegexp *regexp.Regexp
 	Decorate    func(*Setup)
 	Debug       bool
+	Parallel    int
+
+	td errgroup.Group
 }
 
-func (r *Runner) Run() error {
-	for i := range r.Setups {
-		s := &r.Setups[i]
+func (r *Runner) Run(ctx context.Context) error {
+	g, _ := errgroup.WithContext(ctx)
+	if r.Parallel > 0 {
+		g.SetLimit(r.Parallel)
+	}
+	if r.Debug {
+		g.SetLimit(1)
+	}
+
+	setups := slices.Clone(r.Setups)
+	if r.Parallel != 1 {
+		rand.Shuffle(len(setups), func(i, j int) {
+			setups[i], setups[j] = setups[j], setups[i]
+		})
+	}
+
+	for i := range setups {
+		s := &setups[i]
 
 		if r.SetupRegexp != nil && !r.SetupRegexp.MatchString(s.Name) {
 			continue
@@ -40,15 +63,15 @@ func (r *Runner) Run() error {
 		if r.Decorate != nil {
 			r.Decorate(s)
 		}
-		if err := r.runSetup(s); err != nil {
-			return err
-		}
+		g.Go(func() error {
+			return r.runSetup(s)
+		})
 		if r.Debug {
 			break
 		}
 	}
 
-	return nil
+	return multierr.Combine(g.Wait(), r.td.Wait())
 }
 
 func (r *Runner) runSetup(s *Setup) (runErr error) {
@@ -56,6 +79,20 @@ func (r *Runner) runSetup(s *Setup) (runErr error) {
 	if err != nil {
 		return err
 	}
+
+	defer func() {
+		if !r.Debug {
+			r.td.Go(func() error {
+				if err := cmd.Down("-v"); err != nil {
+					return fmt.Errorf("compose down: %w", err)
+				}
+				if err := cmd.Close(); err != nil {
+					return fmt.Errorf("compose close: %w", err)
+				}
+				return nil
+			})
+		}
+	}()
 
 	defer func() {
 		if runErr != nil {
@@ -91,50 +128,28 @@ func (r *Runner) runSetup(s *Setup) (runErr error) {
 	}()
 
 	// Bring up all services except the test service.
-	{
-		args := []string{"-d", "--force-recreate", "--remove-orphans"}
+	args := []string{"-d", "--force-recreate", "--remove-orphans"}
 
-		for name := range s.Compose.Services {
-			if name == TestServiceName {
-				continue
-			}
-			args = append(args, name)
+	for name := range s.Compose.Services {
+		if name == TestServiceName {
+			continue
 		}
+		args = append(args, name)
+	}
 
-		if err := cmd.Up(args...); err != nil {
-			return fmt.Errorf("compose up: %w", err)
-		}
+	if err := cmd.Up(args...); err != nil {
+		return fmt.Errorf("compose up: %w", err)
+	}
 
-		waitTimeout := 10 * time.Second
-		if _, ok := os.LookupEnv("CI"); ok {
-			waitTimeout = 60 * time.Second
-		}
-		if err := cmd.Wait(time.Second, waitTimeout); err != nil {
-			return fmt.Errorf("wait for services: %w", err)
-		}
+	// Wait for services to be ready.
+	waitTimeout := 10 * time.Second
+	if _, ok := os.LookupEnv("CI"); ok {
+		waitTimeout = 60 * time.Second
+	}
+	if err := cmd.Wait(time.Second, waitTimeout); err != nil {
+		return fmt.Errorf("wait for services: %w", err)
 	}
 
 	// Run the test service.
-	{
-		args := []string{"--force-recreate", "--exit-code-from", TestServiceName, TestServiceName}
-
-		if err := cmd.Up(args...); err != nil {
-			return err
-		}
-	}
-
-	// Clean up.
-	if r.Debug {
-		w := os.Stderr
-		fmt.Fprintf(w, "left running at %s\n", cmd.Dir())
-	} else {
-		if err := cmd.Down("-v", "--timeout", "1"); err != nil {
-			return fmt.Errorf("compose down: %w", err)
-		}
-		if err := cmd.Close(); err != nil {
-			return fmt.Errorf("close command: %w", err)
-		}
-	}
-
-	return nil
+	return cmd.Up("--force-recreate", "--exit-code-from", TestServiceName, TestServiceName)
 }
