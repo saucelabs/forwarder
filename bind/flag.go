2c6671178bf1f0d2cc52c03a4b8d18e4b2b79eb3
Michał Matczuk <michal.matczuk@saucelabs.com>
2024-06-25 11:29:45 +0200

    bind: document log file reopening on SIGHUP

--- bind/flag.go
+++ bind/flag.go
@@ -380,7 +380,8 @@ func LogConfig(fs *pflag.FlagSet, cfg *log.Config) {
 	fs.VarP(newOSFileFlag(anyflag.NewValue[*os.File](nil, &cfg.File,
 		forwarder.OpenFileParser(log.DefaultFileFlags, log.DefaultFileMode, log.DefaultDirMode)), &cfg.File),
 		"log-file", "", "<path>"+
-			"Path to the log file, if empty, logs to stdout. ")
+			"Path to the log file, if empty, logs to stdout. "+
+			"The file is reopened on SIGHUP to allow log rotation using external tools. ")
 
 	logLevel := []log.Level{
 		log.ErrorLevel,
