de538b7fcd3e6b48f279cfed399de676647c3548
Michał Matczuk <michal.matczuk@saucelabs.com>
2024-09-03 14:20:26 +0200

    tls: if TLSClientConfig.Insecure is set use all cipher suites
    RSA cipher suites are removed from default set in Go 1.22 github.com/golang/go#63413.
    Add all TLS 1.0 - 1.2 cipher suites if insecure is set to true.

--- tls.go
+++ tls.go
@@ -47,7 +47,36 @@ func DefaultTLSClientConfig() *TLSClientConfig {
 }
 
 func (c *TLSClientConfig) ConfigureTLSConfig(tlsCfg *tls.Config) error {
-	tlsCfg.InsecureSkipVerify = c.Insecure
+	if c.Insecure {
+		tlsCfg.InsecureSkipVerify = true
+		tlsCfg.MinVersion = tls.VersionTLS10
+		// Allow use all cipher suites for insecure connections,
+		// this only affects TLS 1.0-1.2 connections, TLS 1.3 cipher suites are fixed.
+		tlsCfg.CipherSuites = []uint16{
+			tls.TLS_RSA_WITH_RC4_128_SHA,
+			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+			tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+		}
+	}
 
 	if err := c.loadRootCAs(tlsCfg); err != nil {
 		return fmt.Errorf("load CAs: %w", err)
