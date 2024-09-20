da67a52b303d26c7f1bd07236d8881c667301127
Michał Matczuk <michal.matczuk@saucelabs.com>
2024-09-20 15:41:10 +0200

    proxyproto: net.Conn and net.Listener implementations
    The Conn type is designed after tls.Conn.
    We never read more than header bytes from connection.
    This allows to eliminate any 3rd party readers,
    and to use the connection ReadFrom() and WriteTo() functions if provided.
    The underlying connection is available via Conn.NetConn().
    On context cancellation the connection is closed to terminate the paring go routine,
    and to avoid unspecified behaviour caused by double reads from header parsing and user code.

---
+++ proxyproto/net.go
@@ -0,0 +1,152 @@
+// Copyright 2022-2024 Sauce Labs Inc., all rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package proxyproto
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// Conn wraps a net.Conn and provides access to the proxy protocol header.
+// If the header is not present or cannot be read within the timeout,
+// the connection is closed.
+type Conn struct {
+	net.Conn
+
+	readHeaderTimeout time.Duration
+	isHeaderRead      atomic.Bool
+	headerMu          sync.Mutex
+	header            Header
+	headerErr         error
+}
+
+func (c *Conn) NetConn() net.Conn {
+	return c.Conn
+}
+
+func (c *Conn) LocalAddr() net.Addr {
+	if err := c.readHeader(); err != nil {
+		return c.Conn.LocalAddr()
+	}
+
+	if c.headerErr != nil || c.header.IsLocal {
+		return c.Conn.LocalAddr()
+	}
+
+	return c.header.Destination
+}
+
+func (c *Conn) RemoteAddr() net.Addr {
+	if err := c.readHeader(); err != nil {
+		return c.Conn.RemoteAddr()
+	}
+
+	if c.headerErr != nil || c.header.IsLocal {
+		return c.Conn.RemoteAddr()
+	}
+
+	return c.header.Source
+}
+
+func (c *Conn) Read(b []byte) (n int, err error) {
+	if err := c.readHeader(); err != nil {
+		return 0, err
+	}
+	return c.Conn.Read(b)
+}
+
+func (c *Conn) Write(b []byte) (n int, err error) {
+	if err := c.readHeader(); err != nil {
+		return 0, err
+	}
+	return c.Conn.Write(b)
+}
+
+func (c *Conn) Header() (Header, error) {
+	return c.HeaderContext(context.Background())
+}
+
+func (c *Conn) HeaderContext(ctx context.Context) (Header, error) {
+	if err := c.readHeaderContext(ctx); err != nil {
+		return Header{}, err
+	}
+	return c.header, nil
+}
+
+func (c *Conn) readHeader() error {
+	return c.readHeaderContext(context.Background())
+}
+
+func (c *Conn) readHeaderContext(ctx context.Context) error {
+	if c.isHeaderRead.Load() {
+		return c.headerErr
+	}
+
+	c.headerMu.Lock()
+	defer c.headerMu.Unlock()
+
+	if c.isHeaderRead.Load() {
+		return c.headerErr
+	}
+
+	t0 := time.Now()
+	if c.readHeaderTimeout > 0 {
+		if d, ok := ctx.Deadline(); !ok || d.Sub(t0) > c.readHeaderTimeout {
+			var cancel context.CancelFunc
+			ctx, cancel = context.WithTimeout(ctx, c.readHeaderTimeout)
+			defer cancel()
+		}
+	}
+
+	type result struct {
+		header *Header
+		err    error
+	}
+	resCh := make(chan result, 1)
+
+	go func() {
+		var r result
+		r.header, r.err = ReadHeader(c.Conn)
+		resCh <- r
+	}()
+
+	select {
+	case <-ctx.Done():
+		c.Conn.Close()
+		c.headerErr = fmt.Errorf("proxy protocol header read timeout: %w", ctx.Err())
+	case r := <-resCh:
+		if r.header != nil {
+			c.header = *r.header
+		}
+		c.headerErr = r.err
+	}
+
+	c.isHeaderRead.Store(true)
+
+	return c.headerErr
+}
+
+type Listener struct {
+	net.Listener
+	ReadHeaderTimeout time.Duration
+}
+
+func (l *Listener) Accept() (net.Conn, error) {
+	c, err := l.Listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Conn{
+		Conn:              c,
+		readHeaderTimeout: l.ReadHeaderTimeout,
+	}, nil
+}
