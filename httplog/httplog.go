ca36d3ccc6eb858232d160554369f2ad3b795cf6
Hubert Grochowski <hubert.grochowski@saucelabs.com>
2025-03-24 12:54:38 +0100

    httplog: refactor mode validation

--- httplog/httplog.go
+++ httplog/httplog.go
@@ -14,6 +14,7 @@ import (
 	"github.com/saucelabs/forwarder/middleware"
 )
 
+// Mode defines the logging verbosity.
 type Mode string
 
 const (
@@ -43,18 +44,7 @@ func SplitNameMode(val string) (name string, mode Mode, err error) {
 	}
 
 	switch mode {
-	case None:
-		mode = None
-	case ShortURL:
-		mode = ShortURL
-	case URL:
-		mode = URL
-	case Headers:
-		mode = Headers
-	case Body:
-		mode = Body
-	case Errors:
-		mode = Errors
+	case None, ShortURL, URL, Headers, Body, Errors:
 	default:
 		return "", "", fmt.Errorf("invalid mode %q", mode)
 	}
