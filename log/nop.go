18e2dfcfe42e310d94566cd8004865915b0ce9ed
Hubert Grochowski <hubert.grochowski@saucelabs.com>
2025-04-01 09:22:21 +0200

    log: move nopLogger to separate file
    This improves readability and structure.

---
+++ log/nop.go
@@ -0,0 +1,21 @@
+// Copyright 2022-2024 Sauce Labs Inc., all rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package log
+
+// NopLogger is a logger that does nothing.
+var NopLogger = nopLogger{} //nolint:gochecknoglobals // nop implementation
+
+type nopLogger struct{}
+
+func (l nopLogger) Errorf(_ string, _ ...any) {
+}
+
+func (l nopLogger) Infof(_ string, _ ...any) {
+}
+
+func (l nopLogger) Debugf(_ string, _ ...any) {
+}
