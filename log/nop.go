3fca6a22d43e78561d47c8b72dae1ded74c729e6
Hubert Grochowski <hubert.grochowski@saucelabs.com>
2025-04-01 09:58:29 +0200

    log: implement structuredLogger on nop

--- log/nop.go
+++ log/nop.go
@@ -6,16 +6,36 @@
 
 package log
 
+import (
+	"context"
+)
+
 // NopLogger is a logger that does nothing.
 var NopLogger = nopLogger{} //nolint:gochecknoglobals // nop implementation
 
+var (
+	_ Logger           = &nopLogger{}
+	_ StructuredLogger = &nopLogger{}
+)
+
 type nopLogger struct{}
 
-func (l nopLogger) Errorf(_ string, _ ...any) {
-}
+func (l nopLogger) Errorf(_ string, _ ...any) {}
+func (l nopLogger) Infof(_ string, _ ...any)  {}
+func (l nopLogger) Debugf(_ string, _ ...any) {}
 
-func (l nopLogger) Infof(_ string, _ ...any) {
-}
+func (l nopLogger) Fatal(_ string, _ ...any) {}
+func (l nopLogger) Error(_ string, _ ...any) {}
+func (l nopLogger) Warn(_ string, _ ...any)  {}
+func (l nopLogger) Info(_ string, _ ...any)  {}
+func (l nopLogger) Debug(_ string, _ ...any) {}
+func (l nopLogger) Trace(_ string, _ ...any) {}
 
-func (l nopLogger) Debugf(_ string, _ ...any) {
-}
+func (l nopLogger) FatalContext(_ context.Context, _ string, _ ...any) {}
+func (l nopLogger) ErrorContext(_ context.Context, _ string, _ ...any) {}
+func (l nopLogger) WarnContext(_ context.Context, _ string, _ ...any)  {}
+func (l nopLogger) InfoContext(_ context.Context, _ string, _ ...any)  {}
+func (l nopLogger) DebugContext(_ context.Context, _ string, _ ...any) {}
+func (l nopLogger) TraceContext(_ context.Context, _ string, _ ...any) {}
+
+func (l nopLogger) With(_ ...any) StructuredLogger { return l }
