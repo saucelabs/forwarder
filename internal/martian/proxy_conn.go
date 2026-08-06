85d8d25e98eea42ef48ebe718be3bdc0ab6a678a
Sarthak Srivastava <sarthak.srivastava@saucelabs.com>
2026-08-06 07:03:47 +0530

    SC-5585: Correction on an incorrect return line

--- internal/martian/proxy_conn.go
+++ internal/martian/proxy_conn.go
@@ -106,11 +106,11 @@ func (p *proxyConn) readRequest() (*http.Request, error) {
 		log.Error(context.TODO(), "can't set read header deadline", "error", deadlineErr)
 	}
 
-	br, err := sanitizeRequestLine(p.brw.Reader, p.conn)
-	if err != nil {
-		return nil, err
+	if br, err := sanitizeRequestLine(p.brw.Reader, p.conn); err != nil {
+		log.Error(context.TODO(), "can't sanitize request line", "error", err)
+	} else {
+		p.brw.Reader = br
 	}
-	p.brw.Reader = br
 
 	req, err := http.ReadRequest(p.brw.Reader)
 	if err != nil {
