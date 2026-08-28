0dc69bf3b99fe0cd99a969fad43b1633ed31ba17
Michal Rogala <michal.rogala@saucelabs.com>
2026-08-28 14:37:35 +0000

    test

--- http_proxy.go
+++ http_proxy.go
@@ -676,6 +676,10 @@ func (hp *HTTPProxy) Handler() http.Handler {
 	return hp.proxy.Handler()
 }
 
+func (hp *HTTPProxy) HandlerWithIndividualTransport() http.Handler {
+	return hp.proxy.HandlerWithIndividualTransport()
+}
+
 func (hp *HTTPProxy) Run(ctx context.Context) error {
 	if hp.config.TestingHTTPHandler {
 		hp.log.Info("using http handler")
